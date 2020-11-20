24ea19c87fed3ab95463ac96ee4e3b6b2dc6dcd4
jc3213 <jc3213@users.noreply.github.com>
2020-11-20 09:48:23 +0800

    Update HupuBetterHotPost.user.js

--- HupuBetterHotPost.user.js
+++ HupuBetterHotPost.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         优化虎扑热评显示
 // @namespace    https://github.com/jc3213/userscript
-// @version      1.1
+// @version      1
 // @description  限制虎扑热评高度，减少页面占用以优化阅读体验
 // @author       jc3213
 // @match        *://bbs.hupu.com/*
