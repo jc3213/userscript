ad4365230ab2e64536402ec43b0aa2fbac7edf0b
jc3213 <jc3213@users.noreply.github.com>
2020-12-06 01:14:40 +0800

    minor logc change

--- MicroMagazinSurveyNovel.user.js
+++ MicroMagazinSurveyNovel.user.js
@@ -7,42 +7,42 @@
 // @match        *://micromagazine.co.jp/me/qu/*_thanks*.html
 // ==/UserScript==
 
+var book = document.getElementById('books') || document.getElementById('contents_c');
+book.id = 'books';
+
+var css = document.createElement('link');
+css.rel = 'stylesheet';
+css.type = 'text/css';
+css.href = 'style_a.css';
+book.appendChild(css);
+
+var footer;
+var content = book.querySelectorAll('p');
+content.forEach((item, index) => {
+    if (!item.children[0]) {
+        return;
+    }
+    if (index > footer) {
+        item.remove();
+    }
+    else if (item.children[0].tagName === 'BR') {
+        if (index === 0) {
+            item.remove();
+        }
+        item.replaceWith(item.children[0]);
+    }
+    else if (item.children[0].tagName === 'A') {
+        item.remove();
+        footer = index;
+    }
+});
+
 var button = document.createElement('div');
 button.id = 'print_helper';
 button.style.cssText = 'position: absolute; left: 25%; top: 5%; z-lindex: 3213; background-color: white; color: black; padding: 5px; border: 1px solid #e9e9e9; cursor: pointer;';
 button.innerHTML = 'プリント仕様';
 button.addEventListener('click', (event) => {
-    var book = document.getElementById('books') || document.getElementById('contents_c');
-    book.id = 'books';
     book.style.cssText = 'border: none; margin: 0px; padding: 0px';
-
     document.body.replaceWith(book);
-
-    var css = document.createElement('link');
-    css.rel = 'stylesheet';
-    css.type = 'text/css';
-    css.href = 'style_a.css';
-    book.appendChild(css);
-
-    var footer;
-    var content = book.querySelectorAll('p');
-    content.forEach((item, index) => {
-        if (!item.children[0]) {
-            return;
-        }
-        if (index > footer) {
-            item.remove();
-        }
-        else if (item.children[0].tagName === 'BR') {
-            if (index === 0) {
-                item.remove();
-            }
-            item.replaceWith(item.children[0]);
-        }
-        else if (item.children[0].tagName === 'A') {
-            item.remove();
-            footer = index;
-        }
-    });
 });
 document.body.appendChild(button);
