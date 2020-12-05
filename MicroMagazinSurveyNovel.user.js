276f78f771fedfff11a052debaad42e9febdf601
jc3213 <jc3213@users.noreply.github.com>
2020-12-06 01:08:58 +0800

    matching new url and better logic

--- MicroMagazinSurveyNovel.user.js
+++ MicroMagazinSurveyNovel.user.js
@@ -1,32 +1,48 @@
 // ==UserScript==
 // @name         アンケートノベルプリント
 // @namespace    https://github.com/jc3213/userscript
-// @version      3
+// @version      4
 // @description  マイクロマガジン社のアンケート協力者向けの書き下ろしノベルプリントヘルパーです
 // @author       jc3213
-// @match        *://micromagazine.net/me/qu/*_thanks.html
+// @match        *://micromagazine.co.jp/me/qu/*_thanks*.html
 // ==/UserScript==
-
-var css = document.createElement('link');
-css.rel = 'stylesheet';
-css.type = 'text/css';
-css.href = 'style_a.css';
-document.head.appendChild(css);
 
 var button = document.createElement('div');
 button.id = 'print_helper';
 button.style.cssText = 'position: absolute; left: 25%; top: 5%; z-lindex: 3213; background-color: white; color: black; padding: 5px; border: 1px solid #e9e9e9; cursor: pointer;';
 button.innerHTML = 'プリント仕様';
 button.addEventListener('click', (event) => {
-    var content = document.getElementById('books') || document.getElementById('contents_c');
-    content.style.border = 'none';
-    content.style.margin = '0px';
-    content.style.margin = '0px';
-    document.body.replaceWith(content);
+    var book = document.getElementById('books') || document.getElementById('contents_c');
+    book.id = 'books';
+    book.style.cssText = 'border: none; margin: 0px; padding: 0px';
 
-    var header = content.children[0];
-    if (['p', 'br'].includes(header.tagName.toLowerCase())) {
-        header.remove();
-    }
+    document.body.replaceWith(book);
+
+    var css = document.createElement('link');
+    css.rel = 'stylesheet';
+    css.type = 'text/css';
+    css.href = 'style_a.css';
+    book.appendChild(css);
+
+    var footer;
+    var content = book.querySelectorAll('p');
+    content.forEach((item, index) => {
+        if (!item.children[0]) {
+            return;
+        }
+        if (index > footer) {
+            item.remove();
+        }
+        else if (item.children[0].tagName === 'BR') {
+            if (index === 0) {
+                item.remove();
+            }
+            item.replaceWith(item.children[0]);
+        }
+        else if (item.children[0].tagName === 'A') {
+            item.remove();
+            footer = index;
+        }
+    });
 });
 document.body.appendChild(button);
