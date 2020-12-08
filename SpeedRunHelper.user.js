b5a0369d5e4d567f2b03fc5734106b770d78537b
jc3213 <jc3213@users.noreply.github.com>
2020-12-08 13:32:01 +0800

    clean up

--- SpeedRunHelper.user.js
+++ SpeedRunHelper.user.js
@@ -22,9 +22,11 @@ var logger = {};
 var offset = {};
 
 var css = document.createElement('style');
-css.innerHTML = '.speedrun-menu {background-color: #FFF; cursor: pointer; border: 1px outset #F00; padding: 1px; font-size: 14px;}\
-.speedrun-menu:hover {filter: opacity(60%);}\
-.speedrun-menu:active {border: 2px inset #00F; padding: 0px; filter: opacity(30%);}';
+css.innerHTML = '.speedrun-window {position: fixed; z-index: 99999; width: 850px; height: 500px;} \
+.speedrun-menu {background-color: #000; width: 100%; text-align: right; user-select: none;}\
+.speedrun-item {background-color: #fff; cursor: pointer; display: inline-block; text-align: center; vertical-align: center;}\
+.speedrun-item:hover {filter: opacity(60%);}\
+.speedrun-item:active {filter: opacity(30%);}';
 document.head.appendChild(css);
 
 document.querySelectorAll('div[data-ad]').forEach(item => item.remove());
@@ -32,13 +34,13 @@ document.querySelector('body > main > div > div:nth-child(5)').remove();
 
 document.getElementById('leaderboarddiv').addEventListener('contextmenu', (event) => {
     event.preventDefault();
-    var element;
-    document.querySelectorAll('tr[data-target]').forEach(item => { if (item.contains(event.target)) element = item; });
-    if (element) {
-        var src = element.getAttribute('data-target');
-        var id = src.split('/').pop();
-        viewSpeedrunRecord(id, src, event.clientY, event.clientX);
-    }
+    document.querySelectorAll('tr[data-target]').forEach(item => {
+        if (item.contains(event.target)) {
+            var src = item.getAttribute('data-target');
+            var id = src.split('/').pop();
+            viewSpeedrunRecord(id, src, event.clientY, event.clientX);
+        }
+    });
 });
 
 function viewSpeedrunRecord(id, src, top, left) {
@@ -56,9 +58,6 @@ function viewSpeedrunRecord(id, src, top, left) {
                 var xml = document.createElement('div');
                 xml.innerHTML = response.responseText;
                 logger[id] = xml.querySelector('#centerbar').querySelector('iframe');
-                if (!logger[id]) {
-                    logger[id] = xml.querySelector('#centerbar > div > div > center > a').href;
-                }
                 createRecordWindow(id, logger[id], top, left);
             }
         });
@@ -73,19 +72,11 @@ function createRecordWindow(id, content, top, left) {
     var container = document.createElement('div');
     container.id = 'speedrun-' + id;
     container.draggable = 'true';
-    container.style.cssText = 'position: fixed; top: ' + top / 2 + 'px; left: ' + left / 2 + 'px; z-index: 3213; width: 850px; height: 500px;';
+    container.className = 'speedrun-window';
+    container.style.cssText = 'top: ' + top / 2 + 'px; left: ' + left / 2 + 'px;';
+    container.innerHTML = '<div class="speedrun-menu"><span class="speedrun-item">✖</span></div>';
+    container.querySelector('.speedrun-item').addEventListener('click', (event) => container.remove());
     document.body.appendChild(container);
-
-    var menu = document.createElement('div');
-    menu.style.cssText = 'background-color: #000; height: 25px; width: 100%; text-align: right; user-select: none;';
-    container.appendChild(menu);
-
-    var close = document.createElement('span');
-    close.className = 'speedrun-menu';
-    close.addEventListener('click', (event) => container.remove())
-    close.innerHTML = '✖';
-    menu.appendChild(close);
-
     container.appendChild(content);
 }
 
