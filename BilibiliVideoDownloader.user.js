0e0ef9c0e5a368fd3c54bc7289eb0e2bfa61cbd8
jc3213 <jc3213@users.noreply.github.com>
2021-03-02 15:38:20 +0800

    optimized performance

--- BilibiliVideoDownloader.user.js
+++ BilibiliVideoDownloader.user.js
@@ -14,28 +14,28 @@
 // ==/UserScript==
 
 var record = [];
-var format = [
-    {d: '30280', x: 'HQ.aac', r: '音频 高码率'},
-    {d: '30216', x: 'LQ.aac', r: '音频 低码率'},
-    {d: '30120', x: '4K-UHQ.mp4', r: '4K 超清'},
-    {d: '30116', x: '1080HQ.mp4', r: '1080P 60帧'},
-    {d: '30112', x: '1080HBr.mp4', r: '1080P 高码率'},
-    {d: '30080', x: '1080.mp4', r: '1080P 高清'},
-    {d: '30074', x: '720HQ.mp4', r: '720P 60帧'},
-    {d: '30064', x: '720.mp4', r: '720P 高清'},
-    {d: '30032', x: '480.mp4', r: '480P 清晰'},
-    {d: '30016', x: '360.mp4', r: '360P 流畅'},
-    {d: '30015', x: '360LQ.mp4', r: '360P 流畅'},
-    {d: '120', x: '4K-UHQ.flv', r: '4K 超清 FLV'},
-    {d: '116', x: '1080HQ.flv', r: '1080P 60帧 FLV'},
-    {d: '112', x: '1080HBr.flv', r: '1080P 高码率 FLV'},
-    {d: '80', x: '1080.flv', r: '1080P 高清 FLV'},
-    {d: '74', x: '720HQ.flv', r: '720P 60帧 FLV'},
-    {d: '64', x: '720.flv', r: '720P 高清 FLV'},
-    {d: '32', x: '480.flv', r: '480P 清晰 FLV'},
-    {d: '16', x: '360.flv', r: '360P 流畅 FLV'},
-    {d: '15', x: '360LQ.flv', r: '360P 流畅 FLV'},
-];
+var format = {
+    '30280': {x: 'HQ.aac', r: '音频 高码率'},
+    '30216': {x: 'LQ.aac', r: '音频 低码率'},
+    '30120': {x: '4K-UHQ.mp4', r: '4K 超清'},
+    '30116': {x: '1080HQ.mp4', r: '1080P 60帧'},
+    '30112': {x: '1080HBr.mp4', r: '1080P 高码率'},
+    '30080': {x: '1080.mp4', r: '1080P 高清'},
+    '30074': {x: '720HQ.mp4', r: '720P 60帧'},
+    '30064': {x: '720.mp4', r: '720P 高清'},
+    '30032': {x: '480.mp4', r: '480P 清晰'},
+    '30016': {x: '360.mp4', r: '360P 流畅'},
+    '30015': {x: '360LQ.mp4', r: '360P 流畅'},
+    '120': {x: '4K-UHQ.flv', r: '4K 超清 FLV'},
+    '116': {x: '1080HQ.flv', r: '1080P 60帧 FLV'},
+    '112': {x: '1080HBr.flv', r: '1080P 高码率 FLV'},
+    '80': {x: '1080.flv', r: '1080P 高清 FLV'},
+    '74': {x: '720HQ.flv', r: '720P 60帧 FLV'},
+    '64': {x: '720.flv', r: '720P 高清 FLV'},
+    '32': {x: '480.flv', r: '480P 清晰 FLV'},
+    '16': {x: '360.flv', r: '360P 流畅 FLV'},
+    '15': {x: '360LQ.flv', r: '360P 流畅 FLV'}
+};
 
 var btncss = 'background-color: #c26; color: #fff; margin: 1px; padding: 10px; display: inline-block; user-select: none; cursor: pointer;';
 var boxcss = 'position: absolute; z-index: 99999; left: 33%;';
@@ -55,15 +55,19 @@ GM_webRequest([
     {selector: '*://*.bilivideo.cn:*/*', action: 'redirect'},
     {selector: '*://*.cdnnodedns.cn:*/*', action: 'redirect'}
 ], (info, message, details) => {
-    var file = details.url.split('?').shift().split('/').pop();
-    if (record.includes(file) || details.url.includes('data')) {
-        return;
+    try {
+        var file = details.url.match(/1-(\d+)\.m4s/)[1];
+        var type = format[file];
+        if (record.includes(file)) {
+            return;
+        }
+        else if (!type) {
+            return console.log(details.url);
+        }
+        record.push(file);
     }
-    record.push(file);
-
-    var type = format.find(item => item.d === file.split('-').pop().split('.').shift());
-    if (!type) {
-        return console.log(details.url);
+    catch(e) {
+        return;
     }
 
     var item = document.createElement('a');
