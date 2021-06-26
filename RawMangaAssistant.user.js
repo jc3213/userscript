5121dd67823e7f6eb1093f90e7ffcbd724000930
jc3213 <jc3213@users.noreply.github.com>
2021-06-26 15:37:09 +0800

    update ad removal code

--- RawMangaAssistant.user.js
+++ RawMangaAssistant.user.js
@@ -2,7 +2,7 @@
 // @name            Raw Manga Assistant
 // @namespace       https://github.com/jc3213/userscript
 // @name:zh         漫画生肉网站助手
-// @version         5.20
+// @version         5.21
 // @description     Assistant for raw manga online (LoveHug, MangaSum, Komiraw and etc.)
 // @description:zh  漫画生肉网站 (LoveHug, MangaSum, Komiraw 等) 助手脚本
 // @author          jc3213
@@ -25,20 +25,19 @@
 // @webRequest      {"selector": "*.googletagservices.com/*", "action": "cancel"}
 // @webRequest      {"selector": "*.googletagmanager.com/*", "action": "cancel"}
 // @webRequest      {"selector": "*.amazon-adsystem.com/*", "action": "cancel"}
+// @webRequest      {"selector": "*.doubleclick.net/*", "action": "cancel"}
 // @webRequest      {"selector": "*.cloudfront.net/*", "action": "cancel"}
 // @webRequest      {"selector": "*.disqus.com/*", "action": "cancel"}
 // @webRequest      {"selector": "*.facebook.net/*", "action": "cancel"}
 // @webRequest      {"selector": "*.sharethis.com/*", "action": "cancel"}
 //                  lovehug.net
-// @webRequest      {"selector": "*.vidazoo.com/*", "action": "cancel"}
-// @webRequest      {"selector": "*spolecznosci.net/*", "action": "cancel"}
-// @webRequest      {"selector": "*protagcdn.com/*", "action": "cancel"}
-// @webRequest      {"selector": "*.doubleclick.net/*", "action": "cancel"}
 // @webRequest      {"selector": "*.netcatx.com/*", "action": "cancel"}
 // @webRequest      {"selector": "*.aniview.com/*", "action": "cancel"}
-// @webRequest      {"selector": "*defyraspypuke.com/*", "action": "cancel"}
 // @webRequest      {"selector": "*.modoro360.com/*", "action": "cancel"}
 // @webRequest      {"selector": "*.cloud.ovh.net/*", "action": "cancel"}
+// @webRequest      {"selector": "*spolecznosci.net/*", "action": "cancel"}
+// @webRequest      {"selector": "*protagcdn.com/*", "action": "cancel"}
+// @webRequest      {"selector": "*vattingbalak.com/*", "action": "cancel"}
 // komiraw.com / manga11.com / rawdevart.com / kissaway.net
 // @webRequest      {"selector": "*.exdynsrv.com/*", "action": "cancel"}
 //                  manga1000.com / manga1001.com
