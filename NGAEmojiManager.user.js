846ed1bfc1b0da776c5e5718843d54117b20116d
jc3213 <jc3213@users.noreply.github.com>
2021-07-21 23:11:40 +0800

    update storage structure

--- NGAEmojiManager.user.js
+++ NGAEmojiManager.user.js
@@ -2,7 +2,7 @@
 // @name            NGA Emoji Manager
 // @name:zh         NGA表情管理器
 // @namespace       https://github.com/jc3213
-// @version         1.5
+// @version         1.6
 // @description     Add/Remove New Emoji to NGA Forum
 // @description:zh  为NGA论坛添加/删除新表情
 // @author          jc3213
@@ -19,19 +19,25 @@
 // @grant           GM_getValue
 // ==/UserScript==
 
-var emojiPackage = GM_getValue('emoji', {});
+var emojiPackage = GM_getValue('emoji', []);
 var emojiRunOnce = {};
 var emojiTab;
 var emojiPanel;
-var patch = GM_getValue('patch', 1);
+var emojiOrigin;
 
+var patch = GM_getValue('patch', 1);
+var ___package = [];
 if (patch === 1) {
-    Object.keys(emojiPackage).forEach(key => {
-        var emoji = emojiPackage[key];
-        emojiPackage[key] = {emoji};
-    });
+    Object.keys(emojiPackage).forEach(name => ___package.push({name, emoji: emojiPackage[name]}));
+    emojiPackage = ___package;
     GM_setValue('emoji', emojiPackage);
-    GM_setValue('patch', 2);
+    GM_setValue('patch', 3);
+}
+else if (patch === 2) {
+    Object.keys(emojiPackage).forEach(name => ___package.push({name, emoji: emojiPackage[name].emoji, author: emojiPackage[name].author}));
+    emojiPackage = ___package;
+    GM_setValue('emoji', emojiPackage);
+    GM_setValue('patch', 3);
 }
 
 var subscribe = document.createElement('input');
@@ -42,9 +48,11 @@ subscribe.addEventListener('change', (event) => {
         var reader = new FileReader();
         reader.readAsText(file);
         reader.onload = () => {
-            var {name, author, emoji} = JSON.parse(reader.result);
-            emojiPackage[name] = {author, emoji};
-            createEmojiUI(name);
+            var json = JSON.parse(reader.result);
+            var find = emojiPackage.findIndex(package => package.name === json.name);
+            find = find === -1 ? emojiPackage.length : find;
+            emojiPackage[find] = json;
+            createEmojiUI(json, find);
             if (index === files.length - 1) {
                 GM_setValue('emoji', emojiPackage);
             }
@@ -56,45 +64,52 @@ manager.innerText = '添加新表情';
 manager.className = 'block_txt_big';
 manager.addEventListener('click', (event) => subscribe.click());
 
-function createEmojiUI(name) {
-    var tab = document.createElement('button');
-    tab.className = 'block_txt_big';
-    tab.innerText = name;
-    tab.title = '制作者： ' + emojiPackage[name].author;
-    manager.before(tab);
-    var panel = document.createElement('div');
-    emojiPanel.append(panel);
-    tab.addEventListener('click', (event) => {
-        if (event.ctrlKey && confirm('确定要删除【'+ name + '】表情包吗？')) {
-            delete emojiPackage[name];
-            delete emojiRunOnce[name];
-            GM_setValue('emoji', emojiPackage);
-            tab.remove();
-            panel.remove();
-        }
-        else {
-            addEmoji(name, panel);
-        }
-    });
+function createEmojiUI({name, author, emoji}, index) {
+    var panel = emojiPanel.childNodes[index + emojiOrigin];
+    if (panel) {
+        panel.innerHTML = '';
+        addEmoji(name, emoji, panel);
+    }
+    else {
+        var tab = document.createElement('button');
+        tab.className = 'block_txt_big';
+        tab.innerText = name;
+        tab.title = '制作者： ' + author;
+        manager.before(tab);
+        panel = document.createElement('div');
+        emojiPanel.append(panel);
+        tab.addEventListener('click', (event) => {
+            if (event.ctrlKey && confirm('确定要删除【'+ name + '】表情包吗？')) {
+                emojiPackage.splice(index, 1);
+                delete emojiRunOnce[name];
+                GM_setValue('emoji', emojiPackage);
+                tab.remove();
+                panel.remove();
+            }
+            else {
+                emojiPanel.childNodes.forEach(ePanel => {
+                    ePanel.style.display = ePanel === panel ? 'block' : 'none';
+                });
+                if (!emojiRunOnce[name]) {
+                    addEmoji(name, emoji, panel);
+                    emojiRunOnce[name] = true;
+                }
+            }
+        });
+    }
 }
 
-function addEmoji(name, panel) {
-    emojiPanel.childNodes.forEach(ePanel => {
-        ePanel.style.display = ePanel === panel ? 'block' : 'none';
-    });
-    if (!emojiRunOnce[name]) {
-        emojiPackage[name].emoji.forEach(emoji => {
-            var img = document.createElement('img');
-            img.src = 'https://img.nga.178.com/attachments/' + emoji;
-            img.style.cssText = 'max-height: 150px; margin: 0px 5px 5px 0px;';
-            img.addEventListener('click', (event) => {
-                postfunc.addText('[img]' + emoji + '[/img]');
-                postfunc.selectSmilesw._.hide();
-            });
-            panel.appendChild(img);
+function addEmoji(name, emoji, panel) {
+    emoji.forEach(emoji => {
+        var img = document.createElement('img');
+        img.src = 'https://img.nga.178.com/attachments/' + emoji;
+        img.style.cssText = 'max-height: 150px; margin: 0px 5px 5px 0px;';
+        img.addEventListener('click', (event) => {
+            postfunc.addText('[img]' + emoji + '[/img]');
+            postfunc.selectSmilesw._.hide();
         });
-        emojiRunOnce[name] = true;
-    }
+        panel.appendChild(img);
+    });
 }
 
 new MutationObserver(mutationList => {
@@ -103,8 +118,9 @@ new MutationObserver(mutationList => {
         if (newNode && newNode.id.indexOf('commonwindow') !== -1) {
             emojiTab = newNode.querySelector('div > div.div2 > div > div');
             emojiPanel = newNode.querySelector('div > div.div2 > div > span');
+            emojiOrigin = emojiPanel.childNodes.length;
             emojiTab.appendChild(manager);
-            Object.keys(emojiPackage).forEach(createEmojiUI);
+            emojiPackage.forEach(createEmojiUI);
         }
     });
 }).observe(document.body, {childList: true});
