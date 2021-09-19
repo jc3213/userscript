eef22d8c4599fb798427640c5c6638873d2e5668
jc3213 <jc3213@users.noreply.github.com>
2021-09-19 17:21:08 +0800

    oops, download automatically metalink file

--- libs/metalink4.js
+++ libs/metalink4.js
@@ -1,20 +1,31 @@
 (function() {
     this.__metalink4 = {
-        make: (url, filename) {
-            return metaMaker(metaFile({url, filename}));
-            
+        make: (url, filename) => {
+            var file = metaFile({url, filename});
+            var meta = metaMaker(file);
+            metaSaver(meta);
         },
         fromJSON: (json) => {
-            var files = Array.isArray(json) ? json.forEach(fileMarker).join('') : fileMarker(json);
-            return metaMaker(files);
+            var files = Array.isArray(json) ? json.map(metaFile).join('') : metaFile(json);
+            var meta = metaMaker(files);
+            metaSaver(meta);
         }
     }
 
-    function metaFile(filename, url) {
+    function metaFile({url, filename}) {
         return '<file name=' + filename + '"><url>' + url + '</url></file>';
     }
 
     function metaMaker(files) {
         return '<?xml version="1.0" encoding="UTF-8"?><metalink xmlns="urn:ietf:params:xml:ns:metalink">' + files + '</metalink>';
+    }
+
+    function metaSaver(meta) {
+        var blob = new Blob([meta], {type: 'application/metalink+xml; charset=utf-8'});
+        var saver = document.createElement('a');
+        saver.href = URL.createObjectURL(blob);
+        saver.download = 'new_metalink-' + new Date().toLocaleString('ja').replace(/[\/\s:]/g, '_') + '.meta4';
+        saver.click();
+        saver.remove();
     }
 })();
