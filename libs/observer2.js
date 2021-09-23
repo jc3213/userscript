0b29a2373986bacdab817e7e74c22a3e9d67e0e0
jc3213 <jc3213@users.noreply.github.com>
2021-09-23 09:33:58 +0800

    make observer2.js

---
+++ libs/observer2.js
@@ -0,0 +1,48 @@
+(function() {
+    this.__mutation_observer = {
+        node: (node, subtree = false, callback) => {
+            new MutationObserver(mutationList => {
+                mutationList.forEach(mutation => {
+                    var newNode = mutation.addedNodes[0];
+                    if (newNode) {
+                        callback(newNode);
+                    }
+                });
+            }).observe(node, {childList: true, subtree});
+        },
+        attribute: (node, subtree = false, callback) => {
+            new MutationObserver(mutationList => {
+                mutationList.forEach(mutation => {
+                    callback(mutation);
+                });
+            }).observe(node, {attributes: true, subtree});
+        }
+    };
+
+    this.__node_observer = {
+        init: 0,
+        node: (selector, callback) => {
+            this.__node_observer.init = setInterval(() => {
+                var node = document.querySelector(selector);
+                if (node) {
+                    callback(node);
+                    clearInterval(this.__node_observer.init);
+                }
+            })
+        },
+        iframe: (selector1, selector2, callback) => {
+            this.__node_observer.node(selector1, iframe => {
+                this.__node_observer.init = setInterval(() => {
+                    var doc = iframe.contentDocument;
+                    if (doc) {
+                        var node = doc.querySelector(selector2);
+                        if (node) {
+                            callback(node);
+                            clearInterval(this.__node_observer.init);
+                        }
+                    }
+                });
+            });
+        }
+    };
+})();
