2eca22212fca90258e20bc4c8698d75225d281e8
jc3213 <jc3213@users.noreply.github.com>
2021-11-11 23:04:43 +0800

    optimized logic

--- libs/metalink4.js
+++ libs/metalink4.js
@@ -1,8 +1,6 @@
 (function() {
     this.__metalink4 = (i, f, n) => {
-        var blob = i.constructor.name === 'Blob' ? i : new Blob([metaMaker(
-             i.constructor.name === 'Object' ? metaFile(i) : i.constructor.name === 'Array' ? i.map(metaFile).join('') : metaFile({url: i, filename: f})
-        )], {type: 'application/metalink+xml; charset=utf-8'});
+        var blob = i.constructor.name === 'Blob' ? i : n ? __b(i, f) : __b(i);
         var filename = (n ? n : f) ?? 'new_metalink';
         var saver = document.createElement('a');
         saver.href = URL.createObjectURL(blob);
@@ -11,11 +9,13 @@
         saver.remove();
     }
 
-    function metaFile({url, filename}) {
-        return '<file' + (filename ? ' name="' + filename + '"' : '') + '><url>' + url + '</url></file>';
+    function __m(i, f) {
+        return '<file' + (f ? ' name="' + f + '"' : '') + '><url>' + i + '</url></file>';
     }
 
-    function metaMaker(files) {
-        return '<?xml version="1.0" encoding="UTF-8"?><metalink xmlns="urn:ietf:params:xml:ns:metalink">' + files + '</metalink>';
+    function __b(i, f) {
+        var file = f ? __m(i, f) : i.constructor.name === 'Array' ? i.map(e => __m(e.url, e.filename)).join('') : __m(i.url, i.filename);
+        var meta = '<?xml version="1.0" encoding="UTF-8"?><metalink xmlns="urn:ietf:params:xml:ns:metalink">' + file + '</metalink>';
+        return new Blob([meta], {type: 'application/metalink+xml; charset=utf-8'});
     }
 })();
