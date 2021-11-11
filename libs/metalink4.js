9c8a074c79dc592a8ab015fb5b912f7447cf7183
jc3213 <jc3213@users.noreply.github.com>
2021-11-11 21:02:22 +0800

    oops

--- libs/metalink4.js
+++ libs/metalink4.js
@@ -1,16 +1,14 @@
 (function() {
-    this.__metalink4 = {
-        save: (i, f, n) => {
-            var blob = i.constructor.name === 'Blob' ? i : new Blob([metaMaker(
-                 i.constructor.name === 'Object' ? metaFile(i) : i.constructor.name === 'Array' ? i.map(metaFile).join('') : metaFile({url: i, filename: f})
-            )], {type: 'application/metalink+xml; charset=utf-8'});
-            var filename = (n ? n : f) ?? 'new_metalink';
-            var saver = document.createElement('a');
-            saver.href = URL.createObjectURL(blob);
-            saver.download = filename + '-' + new Date().toLocaleString('ja').replace(/[\/\s:]/g, '_') + '.meta4';
-            saver.click();
-            saver.remove();
-        }
+    this.__metalink4 = (i, f, n) => {
+        var blob = i.constructor.name === 'Blob' ? i : new Blob([metaMaker(
+             i.constructor.name === 'Object' ? metaFile(i) : i.constructor.name === 'Array' ? i.map(metaFile).join('') : metaFile({url: i, filename: f})
+        )], {type: 'application/metalink+xml; charset=utf-8'});
+        var filename = (n ? n : f) ?? 'new_metalink';
+        var saver = document.createElement('a');
+        saver.href = URL.createObjectURL(blob);
+        saver.download = filename + '-' + new Date().toLocaleString('ja').replace(/[\/\s:]/g, '_') + '.meta4';
+        saver.click();
+        saver.remove();
     }
 
     function metaFile({url, filename}) {
