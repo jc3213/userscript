7d2679b7b4f278bc9283eb64965dd649b3b7fc67
jc3213 <jc3213@users.noreply.github.com>
2021-11-26 22:57:37 +0800

    Update metalink4.js

--- libs/metalink4.js
+++ libs/metalink4.js
@@ -1,21 +1,19 @@
 (function() {
-    this.__metalink4 = (i, f, n) => {
-        var blob = i.constructor.name === 'Blob' ? i : n ? __b(i, f) : __b(i);
-        var filename = (n ? n : f) ?? 'new_metalink';
+    this.saveAsMetalink = (i, f, n) => fileSaver(i.constructor.name === 'Blob' ? i : n ? makeBlob(i, f) : __b(i), n ? n : f ? f : 'new_metalink');
+
+    function fileSaver(b, n) {
         var saver = document.createElement('a');
-        saver.href = URL.createObjectURL(blob);
-        saver.download = filename + '-' + new Date().toLocaleString('ja').replace(/[\/\s:]/g, '_') + '.meta4';
+        saver.href = URL.createObjectURL(b);
+        saver.download = n + '-' + new Date().toLocaleString('ja').replace(/[\/\s:]/g, '_') + '.meta4';
         saver.click();
         saver.remove();
     }
 
-    function __m(i, f) {
+    function metalink(i, f) {
         return '<file' + (f ? ' name="' + f + '"' : '') + '><url>' + i + '</url></file>';
     }
 
-    function __b(i, f) {
-        var file = f ? __m(i, f) : i.constructor.name === 'Array' ? i.map(e => __m(e.url, e.filename)).join('') : __m(i.url, i.filename);
-        var meta = '<?xml version="1.0" encoding="UTF-8"?><metalink xmlns="urn:ietf:params:xml:ns:metalink">' + file + '</metalink>';
-        return new Blob([meta], {type: 'application/metalink+xml; charset=utf-8'});
+    function makeBlob(i, f) {
+        return new Blob(['<?xml version="1.0" encoding="UTF-8"?><metalink xmlns="urn:ietf:params:xml:ns:metalink">', (f ? metalink(i, f) : i.constructor.name === 'Array' ? i.map(e => metalink(e.url, e.filename)).join('') : metalink(i.url, i.filename)), '</metalink>'], {type: 'application/metalink+xml; charset=utf-8'});
     }
 })();
