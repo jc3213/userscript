cc3e29953b576213b0366a8f805e058801801794
jc3213 <jc3213@users.noreply.github.com>
2021-12-06 22:31:17 +0800

    support new ob5erver2.js

--- BilibiliLiveRoomFilter.user.js
+++ BilibiliLiveRoomFilter.user.js
@@ -2,7 +2,7 @@
 // @name            Bilibili Liveroom Filter
 // @name:zh         哔哩哔哩直播间屏蔽工具
 // @namespace       https://github.com/jc3213/userscript
-// @version         3.1
+// @version         3.2
 // @description     Filtering Bilibili liveroom, batch management, export, import rulelist...
 // @description:zh  哔哩哔哩直播间屏蔽工具，支持管理列表，批量屏蔽，导出、导入列表等……
 // @author          jc3213
@@ -123,7 +123,7 @@ else {
 }
 
 function livePlayerInFrame(id) {
-    __ob5erver2.node({iframe: '#player-ctnr iframe', selector: '#head-info-vm > div > div.rows-ctnr'}, player => {
+    __ob5erver2.node('#head-info-vm > div > div.rows-ctnr', '#player-ctnr iframe', player => {
         player.appendChild(css);
         banInsideLiveRoom(player, id);
     });
