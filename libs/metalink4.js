b4382a6c5eaf4708365ebba04ac550ee7a799fa8
jc3213 <jc3213@users.noreply.github.com>
2021-12-26 19:33:40 +0800

    only recieve object or array, support more details

--- libs/metalink4.js
+++ libs/metalink4.js
@@ -1,19 +1,30 @@
 (function() {
-    this.saveAsMetalink = (i, f, n) => fileSaver(i.constructor.name === 'Blob' ? i : n ? makeBlob(i, f) : makeBlob(i), n ? n : f ? f : 'new_metalink');
-
-    function fileSaver(b, n) {
-        var saver = document.createElement('a');
-        saver.href = URL.createObjectURL(b);
-        saver.download = n + '-' + new Date().toLocaleString('ja').replace(/[\/\s:]/g, '_') + '.meta4';
-        saver.click();
-        saver.remove();
+    this.saveAsMetalink = (i, n) => {
+        var a = Array.isArray(i) ? i : [i];
+        var b = new Blob([
+            '<?xml version="1.0" encoding="UTF-8"?><metalink xmlns="urn:ietf:params:xml:ns:metalink">',
+            ...a.map(m),
+            '</metalink>'
+        ], {type: 'application/metalink+xml; charset=utf-8'});
+        s(b, n);
     }
 
-    function metalink(i, f) {
-        return '<file' + (f ? ' name="' + f + '"' : '') + '><url>' + i + '</url></file>';
+    function s(b, n) {
+        var s = document.createElement('a');
+        s.href = URL.createObjectURL(b);
+        s.download = n + '-' + new Date().toLocaleString('ja').replace(/[\/\s:]/g, '_') + '.meta4';
+        s.click();
+        s.remove();
     }
 
-    function makeBlob(i, f) {
-        return new Blob(['<?xml version="1.0" encoding="UTF-8"?><metalink xmlns="urn:ietf:params:xml:ns:metalink">', ...(f ? [metalink(i, f)] : i.constructor.name === 'Array' ? i.map(e => metalink(e.url, e.filename)) : [metalink(i.url, i.filename)]), '</metalink>'], {type: 'application/metalink+xml; charset=utf-8'});
+    function m({name, size, version, locale, hash, url, metaurl}) {
+        var f = name ? '<file name="' + name + '>' : '<file>';
+        if (size) { f += '<size>' + size + '</size>'; }
+        if (version) { f += '<version>' + version + '</version>'; }
+        if (locale) { f += '<language>' + locale + '</language>'; }
+        if (Array.isArray(hash[0])) { hash.forEach(h => { f += '<hash type="' + h[0] + '">' + h[1] + '</hash>'; }); } else { f += '<hash type="' + hash[0] + '">' + hash[1] + '</hash>'; }
+        if (Array.isArray(url)) { url.forEach(u => { f += '<url location="' + u[0] + '">' + u[1] + '</url>'; }); } else { f += '<url>' + url + '</url>'; }
+        if (Array.isArray(metaurl[0])) { metaurl.forEach(m => { f += '<metaurl metatype="' + m[0] + '">' + m[1] + '</metaurl>'; }); } else { f += '<metaurl metatype="' + metaurl[0] + '">' + metaurl[1] + '</metaurl>'; }
+        return f += '</file>';
     }
 })();
