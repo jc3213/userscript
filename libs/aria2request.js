3cd761ab33a47144c6a6868aed912e076625edd0
jc3213 <jc3213@users.noreply.github.com>
2022-01-01 19:36:52 +0800

    oops, fixed function

--- libs/aria2request.js
+++ libs/aria2request.js
@@ -2,8 +2,8 @@
     this.aria2 = {
         jsonrpc: null,
         secret: null,
-        request: (m, p) => {
-            fetch(aria2.jsonrpc, {method: 'POST', body: JSON.stringify({id: '', jsonrpc: 2, method: m, params: ['token:' + aria2.secret, ...p]})}).then(r => {
+        request: (m, p = []) => {
+            return fetch(aria2.jsonrpc, {method: 'POST', body: JSON.stringify({id: '', jsonrpc: 2, method: m, params: ['token:' + aria2.secret, ...p]})}).then(r => {
                 if (r.ok) { return r.json(); } else { throw new Error(r.statusText); }
             });
         }
