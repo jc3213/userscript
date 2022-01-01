87dfb12d892b29bc664088595b4ac71b7221c8be
jc3213 <jc3213@users.noreply.github.com>
2022-01-01 21:04:46 +0800

    support multi request callback

--- libs/aria2request.js
+++ libs/aria2request.js
@@ -2,9 +2,17 @@
     this.aria2 = {
         jsonrpc: null,
         secret: null,
-        request: (m, p = []) => {
-            return fetch(aria2.jsonrpc, {method: 'POST', body: JSON.stringify({id: '', jsonrpc: 2, method: m, params: ['token:' + aria2.secret, ...p]})})
-                .then(r => { if (r.ok) { return r.json(); } else { throw new Error(r.statusText); } }).then(j => { return 'result' in j ? j.result : j.error; });
-        }
+        send: (m, p) => s({id: '', jsonrpc: 2, method: m, params: ['token:' + aria2.secret].concat(p ?? [])}),
+        multi: (p) => s({id: '', jsonrpc: 2, method: 'system.multicall', params: [p.map(m)]})
     };
+    
+    function s(j) {
+        return fetch(aria2.jsonrpc, {method: 'POST', body: JSON.stringify(j)})
+            .then(r => { if (r.ok) { return r.json(); } else { throw new Error(r.statusText); } })
+            .then(j => { return 'result' in j ? j.result : j.error; });
+    }
+
+    function m({method, params}) {
+        return {methodName: method, params: ['token:' + aria2.secret].concat(params ?? [])};
+    }
 })();
