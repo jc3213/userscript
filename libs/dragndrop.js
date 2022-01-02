4a7d7626b1ecf7686ce5f7ca344789642d8b8435
jc3213 <jc3213@users.noreply.github.com>
2022-01-02 10:11:33 +0800

    Update dragndrop.js

--- libs/dragndrop.js
+++ libs/dragndrop.js
@@ -1,7 +1,20 @@
 (function() {
     this.dragndrop = n => {
-        n.draggable=true,x,y,h=visual.height-n.offsetHeight,w=visual.width-n.offsetWidth;
-        n.addEventListener('dragstart', e => { x=e.clientX,y=e.clientY; });
-        n.addEventListener('dragend', e => {n.style.cssText='top:'+(n.offsetTop+e.clientY-y)+'px;left:'+(n.offsetLeft+e.clientX-x)+'px;';});
+        var x, y;
+        var h = visual.height - n.offsetHeight;
+        var w = visual.width - n.offsetWidth;
+        n.draggable=true;
+        n.addEventListener('dragstart', e => {
+            x = e.clientX;
+            y = e.clientY;
+        });
+        n.addEventListener('dragend', e => {
+            x = n.offsetLeft + e.clientX - x;
+            y = n.offsetTop + e.clientY - y;
+            x= x < 0 ? 0 : x > w ? w : x;
+            y = y < 0 ? 0 : y > h ? h :y;
+            n.style.left = x + 'px';
+            n.style.top = y + 'px';
+        });
     };
 })();
