81c5c61f80a97242067783adbd167f952e1822ad
jc3213 <jc3213@users.noreply.github.com>
2022-01-02 10:56:53 +0800

    oops

--- libs/dragndrop.js
+++ libs/dragndrop.js
@@ -1,18 +1,20 @@
 (function() {
-    this.dragndrop = n => {
-        var x, y;
-        var h = document.documentElement.clientHeight - n.offsetHeight;
-        var w = document.documentElement.clientWidth - n.offsetWidth;
+    this.dragndrop = (n, {width, height}) => {
+        var x, y, w, h;
+        var d = width ?? 0;
+        var t = height ?? 0;
         n.draggable=true;
         n.addEventListener('dragstart', e => {
             x = e.clientX;
             y = e.clientY;
         });
         n.addEventListener('dragend', e => {
+            w = document.documentElement.clientWidth > n.offsetWidth ? document.documentElement.clientWidth - n.offsetWidth : 0;
+            h = document.documentElement.clientHeight > n.offsetHeight ? document.documentElement.clientHeight - n.offsetHeight : 0;
             x = n.offsetLeft + e.clientX - x;
             y = n.offsetTop + e.clientY - y;
-            x= x < 0 ? 0 : x > w ? w : x;
-            y = y < 0 ? 0 : y > h ? h :y;
+            x = x < d ? d : x > w ? w : x;
+            y = y < t ? t : y > h ? h : y;
             n.style.left = x + 'px';
             n.style.top = y + 'px';
         });
