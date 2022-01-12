841a9b215b0efef893fc3768330ee6f8c66cc829
jc3213 <jc3213@users.noreply.github.com>
2022-01-12 19:45:21 +0800

    fixed reaction

--- NyaaTorrentHelper.user.js
+++ NyaaTorrentHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nyaa Torrent Helper
 // @namespace    https://github.com/jc3213/userscript
-// @version      5.3
+// @version      5.4
 // @description  Nyaa Torrent right click to open available open preview in new tab
 // @author       jc3213
 // @connect      *
@@ -46,7 +46,7 @@ if (['502 Bad Gateway', '429 Too Many Requests'].includes(document.title)) {
 var css= document.createElement('style');
 css.innerHTML = '#filter-menu input {display: inline-block; width: 170px; margin-top: 8px;}\
 #filter-menu button {background-color: #056b00; margin-top: -3px;}\
-#filter-list {position: absolute; background-color: #dff0d8; width: 1000px; height: 560px; white-space: nowrap; overflow-y: scroll; display: none; overflow-x: hidden; z-index: 9999999;}\
+#filter-list {position: absolute; background-color: #dff0d8; width: 1000px; height: 560px; white-space: nowrap; overflow-y: scroll; display: none; overflow-x: hidden; z-index: 3213;}\
 #filter-list > * {display: grid; grid-template-columns: 700px 70px 70px 70px 70px; position: relative;}\
 #filter-list > * > * {padding: 10px 5px; margin: 1px; color: #fff; border-radius: 5px; text-decoration: none;}\
 #filter-list > * > *:not(:first-child, text) {text-align: center; cursor: pointer; user-select: none;}\
@@ -56,12 +56,12 @@ css.innerHTML = '#filter-menu input {display: inline-block; width: 170px; margin
 #filter-list > * > *:nth-child(3):not(text) {background-color: #0056b0;}\
 #filter-list > * > *:nth-child(4) {background-color: #056b00;}\
 #filter-list > * > *:nth-child(5) {background-color: #0005cc;}\
-#filter-preview {position: absolute; z-index: 3213;}';
+#filter-preview {position: absolute; z-index: 3213; max-height: 800px; width: auto;}';
 document.head.appendChild(css);
 
 var menu = document.createElement('div');
 menu.id = 'filter-menu';
-menu.innerHTML = '<input class="form-control search-bar" placeholder="' + i18n.keyword + '"><button class="btn btn-primary">🔆</button>';
+menu.innerHTML = '<input class="form-control search-bar" placeholder="' + i18n.keyword + '"><button class="btn btn-primary">🌐</button>';
 document.querySelector('#navbar').appendChild(menu);
 menu.querySelector('input').addEventListener('keypress', event => event.key === 'Enter' && menu.querySelector('button').click());
 menu.querySelector('button').addEventListener('click', event => {
@@ -92,14 +92,15 @@ function getFilterResult(data) {
     '<a href="' + data.magnet + '">' + i18n.magnet + '</a>\
     <span id="copy">' + i18n.copy + '</span>';
     popup.appendChild(menu);
-    menu.querySelector('#preview').addEventListener('click', event => {
+    menu.querySelector('#preview').addEventListener('click', async event => {
+        await getPreviewHandler(data, {top: event.clientY, left: event.clientX});
         event.target.style.cssText = 'background-color: #C3C;';
-        getPreviewHandler(data, {top: event.clientY, left: event.clientX});
     });
-    menu.querySelector('#copy').addEventListener('click', event => {
+    menu.querySelector('#copy').addEventListener('click', async event => {
         event.target.innerText = '!';
-        navigator.clipboard.writeText(i18n.name + ':\n' + data.name + ' (' + data.size + ')\n\n' + i18n.preview + ':\n' + (data.image ? data.image : data.web ? data.web : '') + '\n\n' + (data.torrent ? i18n.torrent + ':\n' + data.torrent + '\n\n' : '') + i18n.magnet + ':\n' + data.magnet);
-        setTimeout(() => {event.target.innerText = i18n.copy;}, 1000);
+        var url = !data.type && await xmlNodeHandler(data.src).then(({url}) => url) ;
+        navigator.clipboard.writeText(i18n.name + ':\n' + data.name + ' (' + data.size + ')\n\n' + i18n.preview + ':\n' + (data.type ? data.url : url ?? '') + '\n\n' + (data.torrent ? i18n.torrent + ':\n' + data.torrent + '\n\n' : '') + i18n.magnet + ':\n' + data.magnet);
+        event.target.innerText = i18n.copy;
     });
 }
 
@@ -112,92 +113,63 @@ document.querySelectorAll('table > tbody > tr').forEach((element) => {
     var src = a.href;
     var size = element.querySelector('td:nth-child(4)').innerText;
     var link = element.querySelectorAll('td:nth-child(3) > a');
-    if (link.length === 2) {
-        var torrent = link[0].href;
-        var magnet = slimMagnetURI(link[1].href);
-    }
-    else {
-        magnet = slimMagnetURI(link[0].href);
-    }
+    var torrent = link.length === 2 ? link[0].href : null;
+    var magnet = link.length === 2 ? link[1].href : link[0].href;
+    magnet = magnet.slice(0, magnet.indexOf('&'));
     var data = {id, name, src, size, torrent, magnet};
     queue.push(data);
-    a.addEventListener('contextmenu', event => {
+    a.addEventListener('contextmenu', async event => {
         event.preventDefault();
+        await getPreviewHandler(data, {top: event.clientY, left: event.clientX});
         a.style.cssText = 'color: #C33;';
-        getPreviewHandler(data, {top: event.clientY, left: event.clientX});
     });
 });
 
-function slimMagnetURI(magnet) {
-    return magnet.slice(0, magnet.indexOf('&'));
-}
-
 // Preview handler
-function getPreviewHandler(data, mouse) {
+async function getPreviewHandler(data, mouse) {
     if (action[data.id] || document.getElementById('preview-' + data.id)) {
         return;
     }
     action[data.id] = true;
-    if (data.none) {
-        noValidPreview(data);
-    }
-    else if (data.image) {
-        createPreview(data, mouse);
-    }
-    else if (data.new) {
-        openWebPreview(data);
-    }
-    else {
-        xmlNodeHandler(data, mouse, getPreviewURL);
-    }
+    data.type === 'none' ? alert(data.name + '\nNo Preview!') :
+    data.type === 'image' ? createPreview(data.url, mouse) :
+    data.type === 'host' ? GM_openInTab(data.url, true) : await xmlNodeHandler(data.src).then(({type, url}) => {
+        data.type = type;
+        data.url = url;
+        action[data.id] = false;
+        getPreviewHandler(data, mouse);
+    }).catch(error => { action[data.id] = getPreviewHandler(data, mouse); });
+    action[data.id] = false;
 }
 
-function xmlNodeHandler(data, mouse, handler) {
-    fetch(data.src).then(response => response.text()).then(text => {
+function xmlNodeHandler(url) {
+    return fetch(url).then(response => response.text()).then(text => {
         if (text.includes('502 Bad Gateway')) {
             throw new Error('502 Bad Gateway');
         }
         else {
             var node = document.createElement('div');
             node.innerHTML = text;
-            handler(node, data, mouse);
+            var desc = node.querySelector('#torrent-description').innerText;
+            var img = /https?:\/\/[^\)\]]+\.(jpg|png)/g.exec(desc);
+            if (img) {
+                return {type: 'image', url: img[0]};
+            }
+            var url = /https?:\/\/[^\*\r\n\)\]]+/g.exec(desc);
+            if (url) {
+                return {type: 'host', url: url[0]};
+            }
+            return {type: 'none'};
         }
-    }).catch(error => setTimeout(() => xmlNodeHandler(data, mouse, handler), 5000));
-}
-
-function getPreviewURL(node, data, mouse) {
-    var description = node.querySelector('#torrent-description').innerHTML;
-    var img = /https?:\/\/[^\)\]]+\.(jpg|png)/g.exec(description);
-    if (img) {
-        data.image = img[0];
-        return createPreview(data, mouse);
-    }
-    var url = /https?:\/\/[^\*\r\n\)\]]+/g.exec(description);
-    if (url) {
-        data.web = url[0];
-        return openWebPreview(data);
-    }
-    data.none = true;
-    noValidPreview(data);
-}
-
-function openWebPreview(data) {
-    GM_openInTab(data.web, true);
-    action[data.id] = false;
-}
-
-function noValidPreview(data) {
-    alert(data.name + '\nNo Preview!');
-    action[data.id] = false;
+    });
 }
 
 // Create preview
-function createPreview(data, mouse) {
+function createPreview(url, mouse) {
     var image = document.createElement('img');
     image.id = 'filter-preview';
-    image.src = data.image;
-    image.style.cssText = 'max-height: 800px; width: auto; top: ' + (mouse.top + 800 > innerHeight ? innerHeight - 800 : mouse.top) + 'px; left: ' + (mouse.left + 600 > innerWidth ? innerWidth - 600 : mouse.left) + 'px;';
+    image.src = url;
+    image.style.cssText = 'top: ' + (mouse.top + 800 > innerHeight ? innerHeight - 800 : mouse.top) + 'px; left: ' + (mouse.left + 600 > innerWidth ? innerWidth - 600 : mouse.left) + 'px;';
     image.addEventListener('click', event => image.remove());
     document.body.appendChild(image);
-    action[data.id] = false;
 }
