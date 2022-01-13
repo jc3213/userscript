935992960525b379625b21365842b399ed092299
jc3213 <jc3213@users.noreply.github.com>
2022-01-13 23:42:33 +0800

    Create filereader.js

---
+++ libs/filereader.js
@@ -0,0 +1,20 @@
+(function() {
+    this.promiseFileReader = i => {
+        var f = new FileReader();
+        return new Promise((r, r) => {
+            r({
+                text: () => s('readAsText'),
+                arrayBuffer: () => s('readAsArrayBuffer'),
+                binary: () => s('readAsBinaryString'),
+                dataURL: () => s('readAsDataURL')
+            });
+        });
+        function s(type) {
+            return new Promise((r, e) => {
+                f.onload = () => r(f.result);
+                f.onerror = e;
+                f[type](i);
+            });
+        }
+    };
+})();
