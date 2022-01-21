962959cdfb3b07a57da2b2b48a7d4796e06a36fe
jc3213 <jc3213@users.noreply.github.com>
2022-01-21 14:41:06 +0800

    minor update

--- libs/aria2request.js
+++ libs/aria2request.js
@@ -8,6 +8,6 @@
     function s(j) {
         return fetch(aria2.jsonrpc, {method: 'POST', body: JSON.stringify(j)})
             .then(r => { if (r.status !== 200) { throw new Error(r.statusText); } return r.json(); })
-            .then(j => { if (j.result[0] && j.result[0].constructor === Object) { throw j.result[0]; } return j.result; });
+            .then(j => { var r = j.result; if (r[0] && r[0].constructor === Object) { throw r[0]; } return r; });
     }
 })();
