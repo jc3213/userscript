a3aef28f7709e2fee77946aeafbfb4c046bf7115
jc3213 <jc3213@users.noreply.github.com>
2022-01-21 14:17:18 +0800

    Update aria2request.js

--- libs/aria2request.js
+++ libs/aria2request.js
@@ -2,17 +2,16 @@
     this.aria2 = {
         jsonrpc: null,
         secret: null,
-        send: (m, p) => s({id: '', jsonrpc: 2, method: m, params: ['token:' + aria2.secret].concat(p ?? [])}),
-        multi: (p) => s({id: '', jsonrpc: 2, method: 'system.multicall', params: [p.map(m)]})
+        send: (r) => s('method' in r ? {id: '', jsonrpc: 2, method: r.method, params: ['token:' + aria2.secret].concat(r.params ?? [])} : m(r));
     };
-    
+
     function s(j) {
         return fetch(aria2.jsonrpc, {method: 'POST', body: JSON.stringify(j)})
-            .then(r => { if (r.ok) { return r.json(); } else { throw new Error(r.statusText); } })
-            .then(j => { if ('result' in j) { return j.result; } else { throw j.error; } });
+            .then(r => { if (r.status !== 200) { throw new Error(r.statusText); } return r.json(); })
+            .then(j => { if (j.resolve[0] && j.result[0].constructor === Object) { throw j.error; } return j.result; });
     }
 
-    function m({method, params}) {
-        return {methodName: method, params: ['token:' + aria2.secret].concat(params ?? [])};
+    function m(p) {
+        return {id: '', jsonrpc: 2, method: 'system.multicall', params: [p.map(({method, params} => ({methodName: method, params: ['token:' + aria2.secret].concat(params ?? [])})))]}
     }
 })();
