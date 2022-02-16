92ce996d34ccb465359bdb12f856f8f7d618a238
jc3213 <jc3213@users.noreply.github.com>
2022-02-16 21:59:10 +0800

    3.0

--- tsdmAutoSignAndWork.user.js
+++ tsdmAutoSignAndWork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         天使动漫自动签到打工
 // @namespace    https://github.com/jc3213/userscript
-// @version      2.6
+// @version      3.0
 // @description  天使动漫全自动打工签到脚本 — 完全自动无需任何操作，只需静待一分钟左右
 // @author       jc3213
 // @match        *://www.tsdm39.net/*
@@ -17,35 +17,33 @@ var autoed = GM_getValue('autoed', false);
 var today = new Date().getFullYear() + new Date().getMonth() + new Date().getDate();
 
 var css = document.createElement('style');
-css.innerHTML = '.my-button {padding: 5px; border: 1px outset #000; text-align: center; vertical-align: middle; display: inline-block; background-color: #FFF; width: 60px; height: 20px; cursor: pointer; font-weight: bold;}\
-.my-button:hover {filter: opacity(60%);}\
-.my-button:active {filter: opacity(30%);}\
-.my-menu {background-color: #fff; position: absolute; top: 15px; left: 100px; z-index: 99999;}'
+css.innerHTML = '.my-menu {background-color: #fff; position: fixed; top: 40px; right: 100px; width: 100px; z-index: 99999;}\
+.my-menu button {width: 100px; padding: 5px;}\
+iframe {position: absolute; top: 200px; left: 100px; height: 800px; width: 800px; display: none;}';
+
 var menu = document.createElement('div');
-menu.innerHTML = '<span class="my-button">签到</span>\
-<span class="my-button">打工</span>\
-<span class="my-button"><span class="my-auto"></span>自动</span>';
+menu.innerHTML = '<button class="my-button">签到</button>\
+<button class="my-button">打工</button>\
+<button class="my-button"><span class="my-auto"></span>自动</button>';
 menu.className = 'my-menu';
 menu.querySelector('.my-auto').innerHTML = autoed ? '✅' : '';
 
-if (document.querySelector('strong.vwmy')) {
-    document.head.appendChild(css);
-    document.body.appendChild(menu);
-    if (autoed) {
-        if (today > signed) {
-            autoHandler('/plugin.php?id=dsu_paulsign:sign', signHandler);
-        }
-        if (Date.now() > worked) {
-            autoHandler('/plugin.php?id=np_cliworkdz:work', workHandler);
-        }
+document.body.append(menu, css);
+
+if (autoed) {
+    if (today > signed) {
+        autoSign();
+    }
+    if (Date.now() > worked) {
+        autoWork();
     }
 }
 
 menu.querySelector('.my-button:nth-child(1)').addEventListener('click', event => {
-    autoHandler('/plugin.php?id=dsu_paulsign:sign', signHandler);
+    autoSign();
 });
 menu.querySelector('.my-button:nth-child(2)').addEventListener('click', event => {
-    autoHandler('/plugin.php?id=np_cliworkdz:work', workHandler);
+    autoWork();
 });
 menu.querySelector('.my-button:nth-child(3)').addEventListener('click', event => {
     autoed = !autoed;
@@ -53,97 +51,78 @@ menu.querySelector('.my-button:nth-child(3)').addEventListener('click', event =>
     GM_setValue('autoed', autoed);
 });
 
-function autoHandler(url, load) {
-    var id = 'auto-' + url.match(/\w{4}$/)[0];
-    if (document.getElementById(id)) {
-        return;
-    }
-    var warn = warnStart(id);
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onload = () => {
-        var node = document.createElement('div');
-        node.innerHTML = xhr.response;
-        load(node, url, id, warn);
-    };
-    xhr.send()
-}
-
-function signHandler(node, url, id, warn) {
-    if (node.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)')) {
+async function autoSign() {
+    var warn = await notification('sign');
+    var frame = await promiseIframe('/plugin.php?id=dsu_paulsign:sign');
+    if (frame.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)')) {
         GM_setValue('signed', today);
-        warnOver(id, node.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)').innerHTML);
+        warn.innerText = frame.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)').innerHTML;
+        autoEnd(frame, warn);
     }
     else {
-        warn.innerHTML = '开始签到...';
-        makeFrame(url, id, autoSign);
+        warn.innerText = '开始签到...';
+        frame.defaultView.Icon_selected('kx');
+        document.getElementById('todaysay').value = '每日签到';
+        setTimeout(() => {
+            frame.defaultView.showWindow('qwindow', 'qiandao', 'post', '0');
+            GM_setValue('signed', today);
+            warn.innerText = '已完成签到';
+            autoEnd(frame, warn);
+        }, 3000);
     }
 }
 
-function workHandler(node, url, id, warn) {
-    if (node.querySelector('#messagetext')) {
-        var text = node.querySelector('#messagetext > p:nth-child(1)').innerHTML.split(/<br>|<script/)[1];
+async function autoWork() {
+    var warn = await notification('work');
+    var frame = await promiseIframe('/plugin.php?id=np_cliworkdz:work');
+    if (frame.querySelector('#messagetext')) {
+        var text = frame.querySelector('#messagetext > p:nth-child(1)').innerHTML.split(/<br>|<script/)[1];
         var clock = text.match(/\d+/g);
         var next = (clock[0] | 0) * 3600000 + (clock[1] | 0) * 60000 + (clock[2] | 0) * 1000;
+        warn.innerText = text;
         if (worked === 0) GM_setValue('worked', Date.now() + next);
-        warnOver(id, text);
+        autoEnd(frame, warn);
     }
     else {
-        warn.innerHTML = '开始打工...';
-        makeFrame(url, id, autoWork);
+        warn.innerText = '开始打工...';
+        frame.querySelectorAll('#advids > div > a').forEach((element, index) => {
+            element.removeAttribute('href');
+            element.removeAttribute('target');
+            setTimeout(() => element.click(), index * 300);
+        });
+        setTimeout(() => {
+            frame.querySelector('#stopad > a').click();
+            GM_setValue('worked', Date.now() + 21600000);
+            warn.innerText = '已完成打工';
+            autoEnd(frame, warn);
+        }, 3000);
     }
 }
 
-function makeFrame(url, id, load) {
-    var iframe = document.createElement('iframe');
-    iframe.src = url;
-    iframe.id = id + '-frame';
-    iframe.style.cssText = 'position: absolute; top: 200px; left: 100px; height: 800px; width: 800px; display: none;';
-    iframe.addEventListener('load', event => load(iframe.contentWindow, iframe.contentDocument));
-    document.body.appendChild(iframe);
-}
-
-function autoSign(window, document) {
-    window.Icon_selected('kx');
-    document.getElementById('todaysay').value = '每日签到';
-    setTimeout(() => {
-        window.showWindow('qwindow', 'qiandao', 'post', '0');
-        GM_setValue('signed', today);
-        warnOver('auto-sign', '已完成签到');
-        window.parent.window.postMessage('auto-sign-frame');
-    }, 3000);
-}
-
-function autoWork(window, document) {
-    document.querySelectorAll('#advids > div > a').forEach((element, index) => {
-        element.removeAttribute('href');
-        element.removeAttribute('target');
-        setTimeout(() => {
-            element.click();
-        }, index * 300);
+function promiseIframe(url) {
+    return new Promise(resolve => {
+        var iframe = document.createElement('iframe');
+        iframe.src = url;
+        iframe.id = url.slice(url.lastIndexOf(':') + 1);
+        iframe.addEventListener('load', event => resolve(iframe.contentDocument));
+        document.body.appendChild(iframe);
     });
+}
+
+function notification(type) {
+    return new Promise(resolve => {
+        var {top, text} = type === 'sign' ? {top: '125px', text: '查询签到状态...'} : {top: '165px', text: '查询打工状态...'};
+        var warn = document.createElement('div');
+        warn.style.cssText = 'border-radius: 5px; background-color: #FFF; padding: 5px; position: fixed; width: 380px; text-align: center; left: ' + (innerWidth - 380) / 2 + 'px; font-size: 16px; top: ' + top;
+        warn.innerHTML = text;
+        document.body.appendChild(warn);
+        resolve(warn)
+    });
+}
+
+function autoEnd(frame, warn) {
     setTimeout(() => {
-        document.querySelector('#stopad > a').click();
-        GM_setValue('worked', Date.now() + 21600000);
-        warnOver('auto-work', '已完成打工');
-        window.parent.window.postMessage('auto-work-frame');
-    }, 3000);
+        frame.defaultView.frameElement.remove();
+        warn.remove();
+    }, 5000);
 }
-
-function warnStart(id) {
-    var data = id === 'auto-sign' ? {top: '125px', text: '查询签到状态...'} : {top: '165px', text: '查询打工状态...'};
-    var warn = document.createElement('div');
-    warn.id = id;
-    warn.style.cssText = 'border-radius: 5px; background-color: #FFF; padding: 5px; position: fixed; width: 380px; text-align: center; left: ' + (innerWidth - 380) / 2 + 'px; font-size: 16px; top: ' + data.top;
-    warn.innerHTML = data.text;
-    document.body.appendChild(warn);
-    return warn;
-}
-
-function warnOver(id, text) {
-    var warn = document.getElementById(id);
-    warn.innerHTML = text;
-    setTimeout(() => warn.remove(), 3000);
-}
-
-window.addEventListener('message', event => setTimeout(() => document.getElementById(event.data).remove(), 5000));
