2ffa93cd982da4c354c08294ef18aaec25f814f8
jc3213 <jc3213@users.noreply.github.com>
2022-09-16 11:27:00 +0800

    v3.4

--- BilibiliLiveRoomFilter.user.js
+++ BilibiliLiveRoomFilter.user.js
@@ -2,7 +2,7 @@
 // @name            Bilibili Liveroom Filter
 // @name:zh         哔哩哔哩直播间屏蔽工具
 // @namespace       https://github.com/jc3213/userscript
-// @version         3.3
+// @version         3.4
 // @description     Filtering Bilibili liveroom, batch management, export, import rulelist...
 // @description:zh  哔哩哔哩直播间屏蔽工具，支持管理列表，批量屏蔽，导出、导入列表等……
 // @author          jc3213
