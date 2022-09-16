f2a9aa0af11b00ddcf51386fdad94cec8d3d95df
jc3213 <jc3213@users.noreply.github.com>
2022-09-16 15:46:13 +0800

    v3.5 update jsui/manager.js

--- BilibiliLiveRoomFilter.user.js
+++ BilibiliLiveRoomFilter.user.js
@@ -2,12 +2,12 @@
 // @name            Bilibili Liveroom Filter
 // @name:zh         哔哩哔哩直播间屏蔽工具
 // @namespace       https://github.com/jc3213/userscript
-// @version         3.4
+// @version         3.5
 // @description     Filtering Bilibili liveroom, batch management, export, import rulelist...
 // @description:zh  哔哩哔哩直播间屏蔽工具，支持管理列表，批量屏蔽，导出、导入列表等……
 // @author          jc3213
 // @match           *://live.bilibili.com/*
-// @require         https://raw.githubusercontent.com/jc3213/jsui/main/manager_0.5.js#sha256-hC0vacAz75BeMQoLyj4/4tHKuh+ZOXWFUtdsVT75uHQ=
+// @require         https://raw.githubusercontent.com/jc3213/jsui/main/manager_0.0.6.js#sha256-HhNRyn4DdiFhZ102HtT2SVXSsfzoWKzHxoZ4xAHJtfs=
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @noframes
@@ -58,15 +58,12 @@ var container = document.createElement('div');
 container.className = 'fancybox';
 container.style.display = 'none';
 
-var manager = new Manager({
-    menu: [
-        {label: '批量屏蔽', onclick: batchBlock},
-        {label: '导入列表', onclick: importList},
-        {label: '导出列表', onclick: exportList},
-        {label: '清空列表', onclick: batchUnblock},
-    ],
-    title: ['直播间ID', '主播昵称']
-});
+var manager = new Manager([
+    {label: '批量屏蔽', onclick: batchBlock},
+    {label: '导入列表', onclick: importList},
+    {label: '导出列表', onclick: exportList},
+    {label: '清空列表', onclick: batchUnblock},
+], ['直播间ID', '主播昵称']);
 manager.parentNode = container;
 function batchBlock(event) {
     if (confirm('确定要屏蔽列表中的直播间吗？')) {
@@ -165,11 +162,16 @@ function banInsideLiveRoom(domPlayer, id) {
 }
 
 function makeBanlist(id, liver) {
-    console.log(id, liver);
-    manager.addNewColumn = {
-        cells: [id, liver],
-        onclick: event => removeBanList(event.target.parentNode, id, liver)
-    };
+    manager.add([id, liver], event => {
+        if (confirm('确定要解除对【 ' + liver + ' 】的屏蔽吗？')) {
+            event.target.parentNode.remove();
+            var index = banned.findIndex(rule => rule.id === id);
+            if (index !== -1) {
+                banned.splice(index, 1);
+            }
+            saveBanlist();
+        }
+    });
 }
 
 function removeBanList(column, id, liver) {
