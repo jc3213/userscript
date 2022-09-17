cb8f7ea87503dce45291fa7cd87ebe5874c5f148
jc3213 <jc3213@users.noreply.github.com>
2022-09-17 13:06:01 +0800

    v7.0 use jsui_menu

--- RawMangaAssistant.user.js
+++ RawMangaAssistant.user.js
@@ -2,7 +2,7 @@
 // @name            Raw Manga Assistant
 // @name:zh         漫画生肉网站助手
 // @namespace       https://github.com/jc3213/userscript
-// @version         6.24
+// @version         7.0
 // @description     Assistant for raw manga online website
 // @description:zh  漫画生肉网站助手脚本
 // @author          jc3213
@@ -15,10 +15,11 @@
 // @match           *://ney5.xyz/*
 // @match           *://mangahatachi.com/*
 // @connect         *
-// @require         https://raw.githubusercontent.com/jc3213/aria2.js/main/src/aria2_0.3.13.js
-// @require         https://raw.githubusercontent.com/jc3213/dragndrop.js/main/src/dragndrop_0.1.0.js
-// @grant           GM_getValue
+// @require         https://raw.githubusercontent.com/jc3213/aria2.js/main/src/aria2_0.3.13.js#sha256-r3LIgBfC9ZzXT6/iO7seAHC3hpRmTHuy1tXsHTV5UPY=
+// @require         https://raw.githubusercontent.com/jc3213/jsui/main/src/menu.js#sha256-JNJVltWNhw5IfbH1dzZG3TuEPUTDGWhmlAjGIWSivyU=
+// @require         https://raw.githubusercontent.com/jc3213/dragndrop.js/main/src/dragndrop_0.1.0.js#sha256-CH+YUPZysVw/cMUTlFCECh491u7VvspceftzLGzhY3g=
 // @grant           GM_setValue
+// @grant           GM_getValue
 // @grant           GM_xmlhttpRequest
 // @grant           GM_webRequest
 // @webRequest      {"selector": "*.googlesyndication.com/*", "action": "cancel"}
@@ -66,12 +67,13 @@ var logo = [];
 var observer;
 var images;
 var watching;
-var options = GM_getValue('options', {contextmenu: 'on'});
-var {jsonrpc = 'http://localhost:6800/jsonrpc', secret = '', iconTop = 350, iconLeft = 200} = options;
+var options = GM_getValue('options', {})
+var {jsonrpc = 'http://localhost:6800/jsonrpc', secret = '', iconTop = 350, iconLeft = 200, ctxMenu = 1} = options;
 var aria2 = new Aria2(jsonrpc, secret);
 var folder;
 var warning;
 var headers = {'cookie': document.cookie, 'referer': location.href, 'user-agent': navigator.userAgent};
+var jsMenu = new JSUI_Menu();
 
 // i18n strings and labels
 var message = {
@@ -86,18 +88,16 @@ var message = {
             done: 'All %n% urls have been <b>copied to clipboard</b>'
         },
         aria2: {
-            label: 'Send to Aria2 RPC',
+            label: 'Send to Aria2',
             done: 'All %n% image urls <b>have been sent to Aria2 RPC</b>',
             error: 'JSONRPC: Failed to send request'
         },
         gotop: {
-            label: 'Back to Top',
-        },
-        lazy: {
-            label: 'Preload All Images'
+            label: 'Scroll to Top',
         },
         menu: {
-            label: 'Context Menu Mode'
+            on: 'Float Button',
+            off: 'Context Menu'
         },
         extract: {
             start: '<b>Extracting</b> manga source',
@@ -117,18 +117,18 @@ var message = {
             done: '%n% 图像链接已<b>复制到剪切板</b>'
         },
         aria2: {
-            label: '发送至 Aria2 RPC',
+            label: '发送至 Aria2',
             done: '全部 %n% 图像链接已发送至<b>Aria2 RPC</b>',
             error: 'JSONRPC: 请求错误'
         },
         gotop: {
             label: '回到顶部',
         },
-        lazy: {
-            label: '预加载所有图像',
+        float: {
+            label: '悬浮菜单'
         },
         menu: {
-            label: '右键菜单模式',
+            label: '右键菜单',
         },
         extract: {
             start: '<b>正在解析</b>图像来源',
@@ -203,7 +203,6 @@ function extractMangaTitle(title = '') {
         else {
             var text = document.querySelector(watching.title.sel).getAttribute(watching.title.attr);
             var temp = watching.title.reg.exec(text);
-            console.log(text, temp)
             title += symbol + temp[watching.title.tl] + symbol + temp[watching.title.ch];
         }
         return title.replace(/[:\?\"\']/g, '_');
@@ -215,47 +214,43 @@ function extractMangaTitle(title = '') {
 
 // Create UI
 var css = document.createElement('style');
-css.innerHTML = '#assistant_button, #assistant_container, #assistant_caution {background-color: #fff; color: #000; position: fixed; z-index: 999999999;}\
-#assistant_button {text-align: center; line-height: 42px; width: 42px !important; height: 42px !important; border: 1px solid darkviolet; top: ' + iconTop + 'px; left: ' + iconLeft + 'px;}\
-#assistant_container {top: ' + iconTop + 'px; left: ' + (iconLeft + 42) + 'px; display: none;}\
-#assistant_container > * {background-color: #fff; width: 220px; border: 1px ridge darkblue; font-size: 14px;}\
-#assistant_container > *:nth-child(-n+2), #assistant_container data {display: none;}\
-#assistant_container.extract > *:nth-child(-n+2), #assistant_container .checked data {display: block;}\
-#assistant_container > * > *, #assistant_caution {line-height: 40px; height: 40px; user-select: none; display: grid; grid-template-columns: 40px auto;}\
-#assistant_container > * > * > *:first-child {text-align: center;}\
-#assistant_caution {font-size: 16px; border: 1px ridge darkviolet; border-radius: 5px; height: 60px; line-height: 60px; text-align: center;}\
-#assistant_caution > *:last-child {text-align: left; width: fit-content; padding-right: 10px;}\
-#assistant_button:hover, #assistant_container > * > *:hover {background-color: darkviolet !important; color: white !important;}';
+css.type = 'text/css';
+css.innerText = '.jsui_button {height: 36px; line-height: 26px; background-color: #fff; color: #000 !important;}\
+.jsui_manager {top: ' + (iconTop) + 'px; left: ' + (iconLeft + 38) + 'px; display: none;}\
+.jsui_manager, #assistant_caution {background-color: #fff; z-index: 999999999; position: fixed;}\
+.jsui_dropdown {border: 1px inset darkviolet; width: 120px;}\
+#assistant_caution {font-size: 16px; color: #000 !important; border: 1px ridge darkviolet; border-radius: 5px; height: 60px; line-height: 60px; text-align: center; padding: 0px 10px;}\
+#assistant_caution > *:last-child {text-align: left; width: fit-content;}';
 
-var button = document.createElement('span');
-button.id = 'assistant_button';
-button.innerHTML = '🖱️';
-button.addEventListener('click', event => {
+var float = jsMenu.button('🖱️', event => {
     container.style.display = 'block';
 });
+float.style.cssText = 'position: fixed; top: ' + iconTop + 'px; left: ' + iconLeft + 'px; width: 38px; height: 38px; z-index: 999999999; border: 1px inset darkviolet;';
 document.addEventListener('click', event => {
-    container.style.display = button.contains(event.target) ? 'block' : 'none';
+    container.style.display = float.contains(event.target) ? 'block' : 'none';
 });
 
 var container = document.createElement('div');
-container.id = 'assistant_container';
-container.innerHTML = '<div><div id="download"><span>💾</span><span>' + i18n.save.label + '</span></div>\
-<div id="clipboard"><span>📄</span><span>' + i18n.copy.label + '</span></div></div>\
-<div><div id="aria2download"><span>🖅</span><span>' + i18n.aria2.label + '</span></div></div>\
-<div><div id="scrolltop"><span>⬆️</span><span>' + i18n.gotop.label + '</div></div>';
-document.body.append(button, container, css);
+container.className = 'jsui_manager';
+document.body.append(float, container, css);
 
 // Draggable button and menu
-var dragndrop = new DragNDrop(button);
+var dragndrop = new DragNDrop(float);
 dragndrop.ondragend = event => {
     container.style.top = dragndrop.offsetTop + 'px';
-    container.style.left = dragndrop.offsetLeft + button.offsetWidth + 'px';
-    iconTop = dragndrop.offsetTop;
-    iconLeft = dragndrop.offsetLeft;
+    container.style.left = dragndrop.offsetLeft + float.offsetWidth + 'px';
+    iconTop = iconTop = dragndrop.offsetTop;
+    iconLeft = iconLeft = dragndrop.offsetLeft;
     GM_setValue('options', {...options, iconTop, iconLeft});
 };
 
-container.querySelector('#download').addEventListener('click', event => {
+var downMenu = jsMenu.menu([
+    {label: i18n.save.label, onclick: downloadAllUrls},
+    {label: i18n.copy.label, onclick: copyAllUrls},
+    {label: i18n.aria2.label, onclick: sendUrlsToAria2}
+], true);
+downMenu.style.display = 'none';
+function downloadAllUrls() {
     urls.forEach((url, index) => {
         GM_xmlhttpRequest({
             url, headers, method: 'GET', responseType: 'blob',
@@ -271,17 +266,13 @@ container.querySelector('#download').addEventListener('click', event => {
             }
         });
     });
-})
-container.querySelector('#clipboard').addEventListener('click', event => {
+}
+function copyAllUrls() {
     navigator.clipboard.writeText(urls.join('\n'));
     notification('copy', 'done');
-});
-container.querySelector('#scrolltop').addEventListener('click', event => {
-    document.documentElement.scrollTop = 0;
-});
-
+}
 // Aria2 Menuitems
-container.querySelector('#aria2download').addEventListener('click', async event => {
+async function sendUrlsToAria2() {
     folder = folder ?? await aria2.message('aria2.getGlobalOption').then(({dir}) => dir + extractMangaTitle()).catch(error => {
         alert(i18n.aria2.error);
         jsonrpc = prompt('Aria2 JSONRPC URI', jsonrpc) ?? jsonrpc;
@@ -293,46 +284,32 @@ container.querySelector('#aria2download').addEventListener('click', async event 
         urls.forEach(async(url, index) => aria2.message('aria2.addUri', [[url], {out: longDecimalNumber(index) + '.' + url.match(/(png|jpg|jpeg|webp)/)[0], dir: folder, ...headers}]));
         notification('aria2', 'done');
     }
-});
+}
 
-// Switchable Menus
-var switchMenu = document.createElement('div');
-[
-    {
-        name: 'contextmenu',
-        on: () => {
-            button.style.display = 'none';
-            document.addEventListener('contextmenu', contextMenuHandler);
-        },
-        off: () => {
-            document.removeEventListener('contextmenu', contextMenuHandler);
-            button.style.display = 'block';
-            container.style.top = button.offsetTop + 'px';
-            container.style.left = button.offsetLeft + button.offsetWidth + 'px';
-        }
-    }
-].forEach(({name, on, off}) => {
-    var menu = document.createElement('div');
-    menu.setAttribute('name', name);
-    menu.innerHTML = '<span><data>✅</data></span><span>' + i18n.menu.label + '</span></div>';
-    menu.addEventListener('click', event => {
-        options[name] = options[name] === 'on' ? 'off' : 'on';
-        switchHandler(menu, name, on, off);
-        GM_setValue('options', options);
-    });
-    switchHandler(menu, name, on, off);
-    switchMenu.appendChild(menu);
-});
-container.appendChild(switchMenu);
-
-function switchHandler(menu, name, on, off) {
-    if (options[name] === 'on') {
-        on();
-        menu.classList.add('checked');
+var modeMenu = jsMenu.menu([
+    {label: i18n.gotop.label, onclick: scrollToTop},
+    {label: i18n.menu.on, onclick: contextMenuMode}
+], true);
+function scrollToTop() {
+    document.documentElement.scrollTop = 0;
+}
+function contextMenuMode() {
+    ctxMenu = ctxMenu === 0 ? 1 : 0;
+    switchMenuMode();
+    GM_setValue('options', {...options, ctxMenu});
+}
+function switchMenuMode() {
+    if (ctxMenu === 1) {
+        float.style.display = 'none';
+        modeMenu.childNodes[1].innerText = i18n.menu.on;
+        document.addEventListener('contextmenu', contextMenuHandler);
     }
     else {
-        off();
-        menu.classList.remove('checked');
+        float.style.display = 'block';
+        modeMenu.childNodes[1].innerText = i18n.menu.off;
+        document.removeEventListener('contextmenu', contextMenuHandler);
+        container.style.top = float.offsetTop + 'px';
+        container.style.left = float.offsetLeft + float.offsetWidth + 'px';
     }
 }
 function contextMenuHandler(event) {
@@ -344,10 +321,12 @@ function contextMenuHandler(event) {
     container.style.left = event.clientX + 'px';
     container.style.display = 'block';
 }
+switchMenuMode();
+
+container.append(downMenu, modeMenu);
 
 // Extract images data
 if (watching) {
-    document.body.removeAttribute('id');
     if (watching.ads) {
         removeAdsElement();
     }
@@ -370,6 +349,7 @@ function removeAdsElement() {
 
 function extractImage() {
     warning = notification('extract', 'start');
+    downMenu.style.display = 'block';
     observer = setInterval(() => {
         if (images.length === urls.length + fail.length + logo.length) {
             warning.remove();
