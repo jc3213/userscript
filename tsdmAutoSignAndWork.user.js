e8f432b1626ebb37f409fa8297c845d849a90e62
jc3213 <jc3213@users.noreply.github.com>
2022-09-21 21:40:12 +0800

    better logic

--- tsdmAutoSignAndWork.user.js
+++ tsdmAutoSignAndWork.user.js
@@ -1,113 +1,129 @@
 // ==UserScript==
 // @name         天使动漫自动签到打工
 // @namespace    https://github.com/jc3213/userscript
-// @version      3.3
+// @version      3.6
 // @description  天使动漫全自动打工签到脚本 — 完全自动无需任何操作，只需静待一分钟左右
 // @author       jc3213
 // @match        *://www.tsdm39.net/*
-// @grant        GM_getValue
-// @grant        GM_setValue
+// @require      https://raw.githubusercontent.com/jc3213/jslib/main/ui/menu.js#sha256-DsH2PJCMq/NhU59epDuDnAZ9CSGYy+059t0xZ/0N98Q=
 // @noframes
 // ==/UserScript==
 
 'use strict';
-var signed = GM_getValue('signed', 0);
-var worked = GM_getValue('worked', 0);
-var autoed = GM_getValue('autoed', false);
-var today = new Date().getFullYear() + new Date().getMonth() + new Date().getDate();
-var now = Date.now();
+var {signed = '0', worked = '0', autorun = '0'} = localStorage;
+var working = {};
+var today = new Date();
+var date = today.getFullYear() + today.getMonth() + today.getDate();
+var now = today.getTime();
+var jsMenu = new JSUI_Menu();
+
+var smenu = jsMenu.menu([
+    {label: '签到', onclick: autoSign},
+    {label: '打工', onclick: autoWork},
+    {label: '自动', onclick: switchAuto}
+]);
+var autoBtn = smenu.childNodes[2];
+autoBtn.innerText = autorun === '1' ? '✅自动' : '自动';
 
 var css = document.createElement('style');
-css.innerHTML = '.my-menu {background-color: #fff; position: fixed; top: 40px; right: 100px; width: 100px; z-index: 99999;}\
-.my-menu button {width: 100px; padding: 5px;}\
-iframe {position: absolute; top: 200px; left: 100px; height: 800px; width: 800px; display: none;}';
+css.innerHTML = '.jsui_basic_menu {width: 180px;}\
+.jsui_menu_btn {font-weight: bold; border-radius: 5px; background-color: #000; padding: 2px 10px; margin: 4px 0px;}\
+.jsui_menu_btn:last-child {flex: 1.5;}\
+iframe {position: absolute; top: 200px; left: 100px; height: 400px; width: 400px; display: none;}';
 
-var menu = document.createElement('div');
-menu.innerHTML = '<button class="my-button">签到</button>\
-<button class="my-button">打工</button>\
-<button class="my-button"><span class="my-auto"></span>自动</button>';
-menu.className = 'my-menu';
-menu.querySelector('.my-auto').innerHTML = autoed ? '✅' : '';
+var headbar = document.querySelector('#toptb > .wp');
+headbar.append(smenu, css);
 
-document.body.append(menu, css);
-
-if (autoed) {
+if (autorun === '1') {
     today > signed && autoSign();
     now > worked ? autoWork() : setTimeout(autoWork, worked - now);
 }
 
-menu.querySelector('.my-button:nth-child(1)').addEventListener('click', autoSign);
-menu.querySelector('.my-button:nth-child(2)').addEventListener('click', autoWork);
-menu.querySelector('.my-button:nth-child(3)').addEventListener('click', event => {
-    autoed = !autoed;
-    menu.querySelector('.my-auto').innerHTML = autoed ? '✅' : '';
-    GM_setValue('autoed', autoed);
-});
+function switchAuto() {
+    localStorage.autorun = autorun = autorun === '1' ? '0' : '1';
+    autoBtn.innerText = autorun === '1' ? '✅自动' : '自动';
+}
 
 async function autoSign() {
-    var {warn, window, document} = await startWork('/plugin.php?id=dsu_paulsign:sign');
-    if (document.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)')) {
-        GM_setValue('signed', today);
-        warn.innerText = document.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)').innerText;
-        endWork(window, warn);
+    if (working.sign) {
+        return;
+    }
+    working.sign = now;
+    var popup = startPopup('查询签到状态...', '80px');
+    var idoc = await startWork('/plugin.php?id=dsu_paulsign:sign');
+    var iwin = idoc.defaultView;
+    if (idoc.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)')) {
+        localStorage.signed = signed = today;
+        popup.innerText = idoc.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)').innerText;
+        endWork(iwin, popup, 'sign');
     }
     else {
-        warn.innerText = '开始签到...';
-        window.Icon_selected('kx');
-        document.getElementById('todaysay').value = '每日签到';
+        popup.innerText = '开始签到...';
+        iwin.Icon_selected('kx');
+        idoc.getElementById('todaysay').value = '每日签到';
         setTimeout(() => {
-            window.showWindow('qwindow', 'qiandao', 'post', '0');
-            GM_setValue('signed', today);
-            warn.innerText = '已完成签到';
-            endWork(window, warn);
+            iwin.showWindow('qwindow', 'qiandao', 'post', '0');
+            localStorage.signed = signed = today;
+            popup.innerText = '已完成签到';
+            endWork(iwin, popup, 'sign');
         }, 3000);
     }
 }
 
 async function autoWork() {
-    var {warn, window, document} = await startWork('/plugin.php?id=np_cliworkdz:work');
-    if (document.querySelector('#messagetext')) {
-        var text = document.querySelector('#messagetext > p:nth-child(1)').innerHTML.split(/<br>|<script/)[1];
+    if (working.work) {
+        return;
+    }
+    working.work = now;
+    var popup = startPopup('查询打工状态...', '120px');
+    var idoc = await startWork('/plugin.php?id=np_cliworkdz:work');
+    var iwin = idoc.defaultView;
+    if (idoc.querySelector('#messagetext')) {
+        var text = idoc.querySelector('#messagetext > p:nth-child(1)').innerHTML.split(/<br>|<script/)[1];
         var clock = text.match(/\d+/g);
         var next = (clock[0] | 0) * 3600000 + (clock[1] | 0) * 60000 + (clock[2] | 0) * 1000;
-        warn.innerText = text;
-        endWork(window, warn);
-        worked === 0 && GM_setValue('worked', Date.now() + next);
+        popup.innerText = text;
+        endWork(iwin, popup, 'work');
+        localStorage.worked = worked = Date.now() + next;
     }
     else {
-        warn.innerText = '开始打工...';
-        document.querySelectorAll('#advids > div > a').forEach((element, index) => {
+        popup.innerText = '开始打工...';
+        idoc.querySelectorAll('#advids > div > a').forEach((element, index) => {
             element.removeAttribute('href');
             element.removeAttribute('target');
             setTimeout(() => element.click(), index * 300);
         });
         setTimeout(() => {
-            document.querySelector('#stopad > a').click();
-            GM_setValue('worked', Date.now() + 21600000);
+            idoc.querySelector('#stopad > a').click();
+            localStorage.worked = worked = Date.now() + 21600000;
             setTimeout(autoWork, 21600000);
-            warn.innerText = '已完成打工';
-            endWork(window, warn);
+            popup.innerText = '已完成打工';
+            endWork(iwin, popup, 'work');
         }, 3000);
     }
 }
 
+function startPopup(string, top) {
+    var popup = document.createElement('div');
+    popup.innerText = string;
+    popup.style.cssText = 'border-radius: 5px; background-color: #FFF; padding: 5px; position: fixed; width: 380px; text-align: center; left: ' + (document.documentElement.clientWidth - 380) / 2 + 'px; font-size: 16px; top: ' + top;
+    document.body.append(popup);
+    return popup;
+}
+
 function startWork(url) {
     return new Promise(resolve => {
-        var type = url.slice(url.lastIndexOf(':') + 1);
         var iframe = document.createElement('iframe');
-        var warn = document.createElement('div');
         iframe.src = url;
-        iframe.addEventListener('load', event => resolve({warn, window: iframe.contentWindow, document: iframe.contentDocument}));
-        var {top, text} = type === 'sign' ? {top: '125px', text: '查询签到状态...'} : {top: '165px', text: '查询打工状态...'};
-        warn.style.cssText = 'border-radius: 5px; background-color: #FFF; padding: 5px; position: fixed; width: 380px; text-align: center; left: ' + (innerWidth - 380) / 2 + 'px; font-size: 16px; top: ' + top;
-        warn.innerHTML = text;
-        document.body.append(iframe, warn);
+        iframe.addEventListener('load', event => resolve(iframe.contentDocument));
+        document.body.append(iframe);
     });
 }
 
-function endWork(window, warn) {
+function endWork(iwin, popup, key) {
     setTimeout(() => {
-        window.frameElement.remove();
-        warn.remove();
+        iwin.frameElement.remove();
+        delete working[key];
+        popup.remove();
     }, 5000);
 }
