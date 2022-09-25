06000fe06cf23a31d426e21a4cc625cee6f7b796
jc3213 <jc3213@users.noreply.github.com>
2022-09-25 22:36:45 +0800

    v1.4.1: update libs

--- BilibiliVideoDownloader.user.js
+++ BilibiliVideoDownloader.user.js
@@ -2,12 +2,12 @@
 // @name            Bilibili Video Downloader
 // @name:zh         哔哩哔哩视频下载器
 // @namespace       https://github.com/jc3213/userscript
-// @version         1.4.0
+// @version         1.4.1
 // @description     Download videos from Bilibili (No Bangumi)
 // @description:zh  下载哔哩哔哩视频（不支持番剧）
 // @author          jc3213
 // @match           *://www.bilibili.com/video/*
-// @require         https://raw.githubusercontent.com/jc3213/jslib/main/ui/menu.js#sha256-rqc3iH2Kcl/OD7aLRrpgVeCmsY4QP1XVqb702NoPAFU=
+// @require         https://raw.githubusercontent.com/jc3213/jslib/7d4380aa6dfc2fcc830791497fb3dc959cf3e49d/ui/menu.js#sha256-/1vgY/GegKrXhrdVf0ttWNavDrD5WyqgbAMMt7MK4SM=
 // @run-at          document-end
 // ==/UserScript==
 
@@ -20,19 +20,19 @@ var title;
 var worker;
 var jsMenu = new FlexMenu();
 var format = {
-    '30280': {label: '音频 高码率', ext: '.192k.aac'},
-    '30232': {label: '音频 中码率', ext: '.128k.aac'},
-    '30216': {label: '音频 低码率', ext: '.64k.aac'},
-    '127': {label: '8K 超高清', ext: '.8K-UHD.mp4'},
-    '120': {label: '4K 超清', ext: '.4K-UHD.mp4'},
-    '116': {label: '1080P 60帧', ext: '.1080HQ.mp4'},
-    '112': {label: '1080P 高码率', ext: '.1080Hbr.mp4'},
-    '80': {label: '1080P 高清', ext: '.1080.mp4'},
-    '74': {label: '720P 60帧', ext: '.720HQ.mp4'},
-    '64': {label: '720P 高清', ext: '.720.mp4'},
-    '32': {label: '480P 清晰', ext: '.480.mp4'},
-    '16': {label: '360P 流畅', ext: '.360.mp4'},
-    '15': {label: '360P 流畅', ext: '.360LQ.mp4'},
+    '30280': {text: '音频 高码率', ext: '.192k.aac'},
+    '30232': {text: '音频 中码率', ext: '.128k.aac'},
+    '30216': {text: '音频 低码率', ext: '.64k.aac'},
+    '127': {text: '8K 超高清', ext: '.8K-UHD.mp4'},
+    '120': {text: '4K 超清', ext: '.4K-UHD.mp4'},
+    '116': {text: '1080P 60帧', ext: '.1080HQ.mp4'},
+    '112': {text: '1080P 高码率', ext: '.1080Hbr.mp4'},
+    '80': {text: '1080P 高清', ext: '.1080.mp4'},
+    '74': {text: '720P 60帧', ext: '.720HQ.mp4'},
+    '64': {text: '720P 高清', ext: '.720.mp4'},
+    '32': {text: '480P 清晰', ext: '.480.mp4'},
+    '16': {text: '360P 流畅', ext: '.360.mp4'},
+    '15': {text: '360P 流畅', ext: '.360LQ.mp4'},
     'avc1': '视频编码: H.264',
     'hev1': '视频编码: HEVC',
     'av01': '视频编码：AV1',
@@ -48,10 +48,12 @@ css.innerHTML = '.jsui-menu-item {background-color: #c26; color: #fff; font-size
 .jsui-options * {font-size: 16px; text-align: center; padding: 5px; width: 100%;}\
 .jsui-options p, .jsui-options option:checked {color: #c26; font-weight: bold;}';
 
-var menu = jsMenu.menu([
-    {label: '设置', onclick: openOptions},
-    {label: '解析', onclick: analyseVideo}
-]);
+var menu = jsMenu.menu({
+    items: [
+        {text: '设置', onclick: openOptions},
+        {text: '解析', onclick: analyseVideo}
+    ]
+});
 menu.style.cssText = 'position: relative; z-index: 9999; width: 160px; ' + offset;
 function openOptions() {
     analyse.style.display = 'none';
@@ -126,9 +128,11 @@ function biliVideoTitle(name) {
 
 function biliVideoThumbnail(url) {
     var fixed = url.replace(/^(https?:)?\/\//, 'https://')
-    var menu = jsMenu.menu([
-        {label: '视频封面', onclick: event => downloadBiliVideo(event, fixed, title + url.slice(url.lastIndexOf('.')))}
-    ], true);
+    var menu = jsMenu.menu({
+        items: [
+            {text: '视频封面', onclick: event => downloadBiliVideo(event, fixed, title + url.slice(url.lastIndexOf('.')))}
+        ], dropdown: true
+    });
     analyse.appendChild(menu);
 }
 
@@ -139,11 +143,12 @@ async function biliVideoExtractor(playurl, key) {
     var {video, audio} = json[key].dash;
     [...video, ...audio].forEach(({id, codecs, baseUrl}) => {
         var codec = codecs.slice(0, codecs.indexOf('.'));
-        var {label, ext} = format[id];
-        menu[codec].push({label, onclick: event => downloadBiliVideo(event, baseUrl, title + ext)});
+        var {text, ext} = format[id];
+        menu[codec].push({text, onclick: event => downloadBiliVideo(event, baseUrl, title + ext)});
     });
-    video = jsMenu.menu(videocodec === '2' ? menu.av01.length !== 0 ? menu.av01 : menu.hev1.length !== 0 ? menu.hev1 : menu.avc1 : videocodec === '1' && menu.hev1.length !== 0 ? menu.hev1 : menu.avc1, true);
-    audio = jsMenu.menu(menu.mp4a, true);
+    var items = videocodec === '2' ? menu.av01.length !== 0 ? menu.av01 : menu.hev1.length !== 0 ? menu.hev1 : menu.avc1 : videocodec === '1' && menu.hev1.length !== 0 ? menu.hev1 : menu.avc1;
+    video = jsMenu.menu({items, dropdown: true});
+    audio = jsMenu.menu({items: menu.mp4a, dropdown: true});
     analyse.append(video, audio);
 }
 
