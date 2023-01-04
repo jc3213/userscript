37c2a4a83a4d0838ad0eea6a06f0a67c979f8f43
jc3213 <jc3213@users.noreply.github.com>
2023-01-04 10:33:02 +0800

    v1.4.10: update libs

--- BilibiliVideoDownloader.user.js
+++ BilibiliVideoDownloader.user.js
@@ -2,12 +2,12 @@
 // @name            Bilibili Video Downloader
 // @name:zh         哔哩哔哩视频下载器
 // @namespace       https://github.com/jc3213/userscript
-// @version         1.4.9
+// @version         1.4.10
 // @description     Download videos from Bilibili (No Bangumi)
 // @description:zh  下载哔哩哔哩视频（不支持番剧）
 // @author          jc3213
 // @match           *://www.bilibili.com/video/*
-// @require         https://raw.githubusercontent.com/jc3213/jslib/3aa59ec35171169068f63703a76799524e32ec48/js/jsui.js#sha256-XhP7/w7IFRLG3eoySzn5pxbd1Is6hClCyuAEwFDwSn8=
+// @require         https://raw.githubusercontent.com/jc3213/jslib/ca80d098c368bb50dd8392d6b83228db00adb4ad/js/jsui.js#sha256-20PdtNwTYCv+5Q/GHM/ydyUpKBVwsQtlYVQZkLQwnvU=
 // @grant           GM_webRequest
 // @grant           GM_download
 // @webRequest      {"selector": "*://s1.hdslb.com/bfs/static/jinkela/long/js/sentry/*", "action": "cancel"}
@@ -155,7 +155,7 @@ async function biliVideoExtractor(name, image, playurl, key) {
         var codec = codecs.slice(0, codecs.indexOf('.'));
         var {text, ext} = format[id];
         var {title, alt} = format[codec];
-        menu[codec].push({text, onclick: event => downloadBiliVideo(event, baseUrl, alt + ext), attributes: [{name: 'title', value: title}]});
+        menu[codec].push({text, onclick: event => downloadBiliVideo(event, baseUrl, alt + ext), attr: [{name: 'title', value: title}]});
     });
     var vid = videocodec === '2' ? menu.av01.length !== 0 ? menu.av01 : menu.hev1.length !== 0 ? menu.hev1 : menu.avc1 : videocodec === '1' && menu.hev1.length !== 0 ? menu.hev1 : menu.avc1;
     video = jsUI.menulist(vid, true);
