41423446ba004b9f382a50b40332a90b9ef9a8b4
jc3213 <jc3213@users.noreply.github.com>
2023-04-03 19:06:21 +0800

    Create jsDelivrLinkForGithub.user.js

---
+++ jsDelivrLinkForGithub.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         jsDelivr link for Github
+// @namespace    https://github.com/jc3213/userscript
+// @version      0.1.0
+// @description  Add a button to copy jsdelivr link for github files
+// @author       jc3213
+// @match        https://github.com/*/*/blob/*
+// @match        https://github.com/*/*/commit/*
+// @require      https://cdn.jsdelivr.net/gh/jc3213/jslib@ceaca1a2060344909a408a1e157e3cd23e4dbfe0/js/nodeobserver.js#sha256-R3ptp1LZaBZu70+IAJ9KX1CJ7BN4wyrANtHb48wlROc=
+// @run-at       document-idle
+// ==/UserScript==
+
+var {pathname} = location;
+var components = pathname.split('/');
+var [none, username, repository, whatis] = components.splice(0, 4);
+var jsdelivr = 'https://cdn.jsdelivr.net/gh/' + username + '/' + repository + '@';
+var stylecss;
+
+if (pathname.includes('blob')) {
+    stylecss = 'scale: 1.5;';
+    newNodeTimeoutObserver('#spoof-warning + div + div > div > :nth-child(2) > :nth-child(2) > :nth-child(2)').then(createJSDelivrButton);
+}
+else if (pathname.includes('commit')) {
+    stylecss = 'scale: 2; top: 10px;';
+    newNodeTimeoutObserver('div.js-diff-progressive-container').then(async files => {
+        files.querySelectorAll('span.Truncate').forEach(createJSDelivrButton);
+    });
+}
+
+function createJSDelivrButton(menu) {
+    var copy = document.createElement('div');
+    copy.className = 'd-inline-block btn-octicon';
+    copy.innerText = '🖹';
+    copy.title = 'Copy jsdelivr link'
+    copy.style.cssText = 'position: relative; user-select: none; cursor: pointer; ' + stylecss;
+    copy.addEventListener('click', event => {
+        if (whatis === 'blob') {
+            copy.url = components.join('/');
+        }
+        else if (whatis === 'commit') {
+            copy.url = [...components].pop() + '/' + menu.querySelector('a').title;
+        }
+        navigator.clipboard.writeText(jsdelivr + copy.url);
+    });
+    menu.appendChild(copy)
+}
