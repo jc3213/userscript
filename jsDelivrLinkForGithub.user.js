4cae538a304946ec851b622dcbf62824ad593e8e
jc3213 <jc3213@users.noreply.github.com>
2023-04-05 11:04:56 +0800

    v0.3.0: fixed logic, add more event

--- jsDelivrLinkForGithub.user.js
+++ jsDelivrLinkForGithub.user.js
@@ -1,46 +1,59 @@
 // ==UserScript==
 // @name         jsDelivr link for Github
 // @namespace    https://github.com/jc3213/userscript
-// @version      0.1.0
+// @version      0.3.0
 // @description  Add a button to copy jsdelivr link for github files
 // @author       jc3213
-// @match        https://github.com/*/*/blob/*
-// @match        https://github.com/*/*/commit/*
+// @match        https://github.com/*
 // @require      https://cdn.jsdelivr.net/gh/jc3213/jslib@ceaca1a2060344909a408a1e157e3cd23e4dbfe0/js/nodeobserver.js#sha256-R3ptp1LZaBZu70+IAJ9KX1CJ7BN4wyrANtHb48wlROc=
 // @run-at       document-idle
 // ==/UserScript==
 
-var {pathname} = location;
-var components = pathname.split('/');
-var [none, username, repository, whatis] = components.splice(0, 4);
-var jsdelivr = 'https://cdn.jsdelivr.net/gh/' + username + '/' + repository + '@';
+var where;
+var whatis;
+var components;
 var stylecss;
+var jsdelivr;
 
-if (pathname.includes('blob')) {
-    stylecss = 'scale: 1.5;';
-    newNodeTimeoutObserver('#spoof-warning + div + div > div > :nth-child(2) > :nth-child(2) > :nth-child(2)').then(createJSDelivrButton);
-}
-else if (pathname.includes('commit')) {
-    stylecss = 'scale: 2; top: 10px;';
-    newNodeTimeoutObserver('div.js-diff-progressive-container').then(async files => {
-        files.querySelectorAll('span.Truncate').forEach(createJSDelivrButton);
-    });
-}
+new MutationObserver(mutations => {
+    if (where !== location.pathname) {
+        where = location.pathname;
+        components = where.split('/');
+        whatis = components[3];
+        jsdelivr = 'https://cdn.jsdelivr.net/gh/' + components[1] + '/' + components[2] + '@';
+        components.splice(0, 4);
+        if (whatis === 'blob') {
+            stylecss = 'scale: 1.5;';
+            newNodeTimeoutObserver('#spoof-warning + div + div > div > :nth-child(2) > :nth-child(2) > :nth-child(2)').then(createJSDelivrButton);
+        }
+        else if (whatis === 'commit') {
+            stylecss = 'scale: 2; top: 10px;';
+            newNodeTimeoutObserver('div.js-diff-progressive-container').then(async files => {
+                files.querySelectorAll('span.Truncate').forEach(createJSDelivrButton);
+            });
+        }
+    }
+}).observe(document.head, {childList: true});
 
 function createJSDelivrButton(menu) {
     var copy = document.createElement('div');
+    if (whatis === 'blob') {
+        copy.url = jsdelivr + components.join('/');
+    }
+    else if (whatis === 'commit') {
+        copy.url = jsdelivr + [...components].pop() + '/' + menu.querySelector('a').title;
+    }
     copy.className = 'd-inline-block btn-octicon';
     copy.innerText = '🖹';
     copy.title = 'Copy jsdelivr link'
     copy.style.cssText = 'position: relative; user-select: none; cursor: pointer; ' + stylecss;
-    copy.addEventListener('click', event => {
-        if (whatis === 'blob') {
-            copy.url = components.join('/');
+    copy.addEventListener('mousedown', ({button}) => {
+        if (button === 0) {
+            navigator.clipboard.writeText(copy.url);
         }
-        else if (whatis === 'commit') {
-            copy.url = [...components].pop() + '/' + menu.querySelector('a').title;
+        else if (button === 1) {
+            open(copy.url);
         }
-        navigator.clipboard.writeText(jsdelivr + copy.url);
     });
     menu.appendChild(copy)
 }
