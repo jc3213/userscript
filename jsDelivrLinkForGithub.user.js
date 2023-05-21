478fe9b67524ec80ed86bfe1ae0da3ffabece6cf
jc3213 <jc3213@users.noreply.github.com>
2023-05-21 09:01:20 +0800

    v0.9: fixed menu for long commit view

--- jsDelivrLinkForGithub.user.js
+++ jsDelivrLinkForGithub.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         jsDelivr link for Github
 // @namespace    https://github.com/jc3213/userscript
-// @version      0.8.1
+// @version      0.9
 // @description  Add a button to copy jsdelivr link for github files
 // @author       jc3213
 // @match        https://github.com/*
-// @require      https://cdn.jsdelivr.net/gh/jc3213/jslib@ceaca1a2060344909a408a1e157e3cd23e4dbfe0/js/nodeobserver.js#sha256-R3ptp1LZaBZu70+IAJ9KX1CJ7BN4wyrANtHb48wlROc=
+// @require      https://cdn.jsdelivr.net/gh/jc3213/jslib@7609e22d5506c10182773660667136e9b96fe744/js/nodeobserver.js#sha256-xG7yfLlwtkpejTuRCKVeI7LJPUtx+SvAtnjMhsqnHbM=
 // @run-at       document-idle
 // ==/UserScript==
 
@@ -13,9 +13,8 @@ var where;
 var whatis;
 var components;
 var jsdelivr;
-var tagName;
-var cssText;
-var className;
+var button = {};
+var observer = new NodeObserver();
 
 new MutationObserver(mutations => {
     if (where !== location.pathname) {
@@ -35,29 +34,34 @@ new MutationObserver(mutations => {
 }).observe(document.head, {childList: true});
 
 function newCodeView() {
-    tagName = 'button';
-    cssText = 'font-size: 20px; height: 28px;';
-    className = 'types__StyledButton-sc-ws60qy-0 kbjJSF';
-    newNodeTimeoutObserver('.react-blob-header-edit-and-raw-actions').then(findJSDelivrButton);
+    button.tagName = 'button';
+    button.cssText = 'font-size: 20px; height: 28px;';
+    button.className = 'types__StyledButton-sc-ws60qy-0 kbjJSF';
+    observer.timeout('.react-blob-header-edit-and-raw-actions').then(findJSDelivrButton);
 }
 
 function oldCodeView() {
-    tagName = 'remote-clipboard-copy';
-    cssText = 'scale: 1.66;';
-    className = 'd-inline-block btn-octicon';
-    newNodeTimeoutObserver('.BtnGroup + .d-flex > .ml-1 + div').then(createJSDelivrButton);
+    button.tagName = 'remote-clipboard-copy';
+    button.cssText = 'scale: 1.66;';
+    button.className = 'd-inline-block btn-octicon';
+    observer.timeout('.BtnGroup + .d-flex > .ml-1 + div').then(createJSDelivrButton);
 }
 
-function commitView() {
-    tagName = 'clipboard-copy';
-    cssText = 'scale: 1.92; top: 9px; left: -8px;';
-    className = 'd-inline-block btn-octicon';
-    newNodeTimeoutObserver('div.js-diff-progressive-container').then(async files => {
-        files.querySelectorAll('span.Truncate').forEach(menu => {
-            var button = createJSDelivrButton(menu);
-            button.url += button.previousElementSibling.value;
-        });
+async function commitView() {
+    button.tagName = 'clipboard-copy';
+    button.cssText = 'scale: 1.92; top: 9px; left: -8px;';
+    button.className = 'd-inline-block btn-octicon';
+    var files = await observer.timeout('#files');
+    files.querySelectorAll('span.Truncate').forEach(commitMenu);
+    observer.mutation(files, {tagName: 'DIV'}, file => {
+        var menu = file.querySelector('span.Truncate');
+        commitMenu(menu);
     });
+}
+
+function commitMenu(menu) {
+    var button = createJSDelivrButton(menu);
+    button.url += button.previousElementSibling.value;
 }
 
 function findJSDelivrButton(menu) {
@@ -71,6 +75,7 @@ function findJSDelivrButton(menu) {
 }
 
 function createJSDelivrButton(menu) {
+    var {tagName, className, cssText} = button;
     var copy = document.createElement(tagName);
     copy.url = jsdelivr;
     copy.className = className;
