dacbd9751dc6dd50dba68a9cdbe68046242155f2
jc3213 <jc3213@users.noreply.github.com>
2024-09-24 07:41:16 +0800

    v1.4.0: better logic

--- tsdmAutoSignAndWork.user.js
+++ tsdmAutoSignAndWork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         天使动漫自动签到打工
 // @namespace    https://github.com/jc3213/userscript
-// @version      1.3.0
+// @version      1.4.0
 // @description  天使动漫全自动打工签到脚本 — 完全自动无需任何操作，只需静待一分钟左右
 // @author       jc3213
 // @match        *://*.tsdm39.com/*
@@ -14,12 +14,9 @@ var action = {};
 var today = new Date();
 var date = today.getFullYear() + today.getMonth() + today.getDate();
 var now = today.getTime();
+var login = document.getElementById('lsform');
 
-var css = document.createElement('style');
-css.innerHTML = `iframe {position: absolute; top: 200px; left: 100px; height: 400px; width: 400px; display: none;}`;
-document.head.appendChild(css);
-
-if (!document.querySelector('#lsform')) {
+if (!login) {
     if (date > signed) {
         autoSign();
     }
@@ -46,21 +43,20 @@ async function autoSign() {
     }
     action.signed = true;
     var popup = startPopup('查询签到状态...', '80px');
-    var idoc = await startWork('/plugin.php?id=dsu_paulsign:sign');
-    var iwin = idoc.defaultView;
-    var error = idoc.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)');
+    var iframe = await startWork('/plugin.php?id=dsu_paulsign:sign');
+    var error = iframe.document.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)');
     if (error) {
         var text = error.innerText;
     }
     else {
         popup.innerText = '开始签到...';
-        iwin.Icon_selected('kx');
-        idoc.getElementById('todaysay').value = '每日签到';
+        iframe.window.Icon_selected('kx');
+        iframe.document.getElementById('todaysay').value = '每日签到';
         await waitTimeout(3000);
-        iwin.showWindow('qwindow', 'qiandao', 'post', '0');
+        iframe.window.showWindow('qwindow', 'qiandao', 'post', '0');
         text = '已完成签到';
     }
-    endWork('signed', date, text, popup, iwin);
+    endWork('signed', date, text, popup, iframe.window);
 }
 
 async function autoWork() {
@@ -69,29 +65,28 @@ async function autoWork() {
     }
     action.worked = true;
     var popup = startPopup('查询打工状态...', '120px');
-    var idoc = await startWork('/plugin.php?id=np_cliworkdz:work');
-    var iwin = idoc.defaultView;
-    if (idoc.querySelector('#messagetext')) {
-        var text = idoc.querySelector('#messagetext > p:nth-child(1)').innerHTML.split(/<br>|<script/)[1];
+    var iframe = await startWork('/plugin.php?id=np_cliworkdz:work');
+    if (iframe.document.querySelector('#messagetext')) {
+        var text = iframe.document.querySelector('#messagetext > p:nth-child(1)').innerHTML.split(/<br>|<script/)[1];
         var [full, hh, mm, ss] = text.match(/(\d)小时(\d+)分钟(\d+)秒/);
         var next = hh * 3600000 + mm * 60000 + ss * 1000;
     }
     else {
         popup.innerText = '开始打工...';
-        idoc.querySelectorAll('#advids > div > a').forEach(async (element, index) => {
+        iframe.document.querySelectorAll('#advids > div > a').forEach(async (element, index) => {
             element.removeAttribute('href');
             element.removeAttribute('target');
             await waitTimeout(index * 300);
             element.click();
         });
         await waitTimeout(3000);
-        idoc.querySelector('#stopad > a').click();
+        iframe.document.querySelector('#stopad > a').click();
         setTimeout(autoWork, 21600000);
         text = '已完成打工';
         next = 21600000;
 
     }
-    endWork('worked', Date.now() + next, text, popup, iwin);
+    endWork('worked', Date.now() + next, text, popup, iframe.window);
 }
 
 async function endWork(work, value, text, popup, frame) {
@@ -115,14 +110,13 @@ function startWork(url) {
     return new Promise(resolve => {
         var iframe = document.createElement('iframe');
         iframe.src = url;
-        iframe.addEventListener('load', event => resolve(iframe.contentDocument));
+        iframe.style.cssText = 'position: absolute; top: 400px; left: 400px; height: 400px; width: 400px; display: none;';
+        iframe.addEventListener('load', event => resolve({document: iframe.contentDocument, window: iframe.contentWindow}));
         document.body.append(iframe);
-        iframe.contentWindow.setTimeout = () => void 0;
+        iframe.contentWindow.setTimeout = () => null;
     });
 }
 
 function waitTimeout(number) {
-    return new Promise(resolve => {
-        setTimeout(resolve, number);
-    });
+    return new Promise((resolve) => setTimeout(resolve, number));
 }
