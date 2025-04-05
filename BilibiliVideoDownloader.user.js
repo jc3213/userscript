d504204699b6bd24232ca18900aa3a4930779e91
jc3213 <jc3213@users.noreply.github.com>
2025-04-05 12:28:22 +0800

    v1.8.0: 重写脚本

--- BilibiliVideoDownloader.user.js
+++ BilibiliVideoDownloader.user.js
@@ -2,25 +2,23 @@
 // @name            Bilibili Video Downloader
 // @name:zh         哔哩哔哩视频下载器
 // @namespace       https://github.com/jc3213/userscript
-// @version         1.7.9
+// @version         1.8.0
 // @description     Download videos from Bilibili (No Bangumi)
 // @description:zh  下载哔哩哔哩视频（不支持番剧）
 // @author          jc3213
 // @match           *://www.bilibili.com/video/*
 // @match           *://www.bilibili.com/v/*
-// @require         https://cdn.jsdelivr.net/gh/jc3213/jslib@e7814b44512263b5e8125657aff4c1be5fe093a5/ui/jsui.min.js#sha256-mnAxgBFxrf9LCVUKhR2ikxUBvTY0/sFs9wjF3kDV9Mg=
-// @require         https://cdn.jsdelivr.net/gh/jc3213/jslib@7cb4fb4348574f426417490c20e0ea7d8f0b3187/js/nodeobserver.js#sha256-v48u9yZlthnR8qPvz1AEnK7WLtQmn56wKT1qX76Ic+w=
 // @grant           GM_download
 // @run-at          document-idle
 // ==/UserScript==
 
-var {autowide = '0', videocodec = '0'} = localStorage;
-var watch = location.pathname;
-var worker = true;
-var title;
-var history = {};
-var archive;
-var format = {
+let {autowide = '0', videocodec = '0'} = localStorage;
+let bvTitle;
+let bvWatch = location.pathname;
+let worker = true;
+let history = {};
+let archive;
+let format = {
     '30280': {text: '音频 高码率', ext: '.192k.m4a'},
     '30232': {text: '音频 中码率', ext: '.128k.m4a'},
     '30216': {text: '音频 低码率', ext: '.64k.m4a'},
@@ -34,169 +32,235 @@ var format = {
     '32': {text: '480P 清晰', ext: '.480.mp4'},
     '16': {text: '360P 流畅', ext: '.360.mp4'},
     '15': {text: '360P 流畅', ext: '.360lq.mp4'},
-    'avc1': {title: '视频编码: H.264', alt: '.h264'},
-    'hev1': {title: '视频编码: HEVC', alt: '.h265'},
-    'av01': {title: '视频编码：AV1', alt: '.av1'},
-    'mp4a': {title: '音频编码: AAC', alt: '.aac'}
+    'avc1': {title: '视频编码: H.264', alt: 'h264', type: 'video'},
+    'hev1': {title: '视频编码: HEVC', alt: 'h265', type: 'video'},
+    'av01': {title: '视频编码：AV1', alt: 'av1', type: 'video'},
+    'mp4a': {title: '音频编码: AAC', alt: 'aac', type: 'audio'}
 };
 
-if (watch.startsWith('/video/')) {
-    var bvplayer = true;
-    var menuBox = 'div.video-toolbar-left';
-    var wideBtn = 'div.bpx-player-ctrl-wide';
-    var wideStat = 'bpx-state-entered';
-    var current = 'li.bpx-state-multi-active-item';
-    var offset = `.jsui-video-menu {position: relative;}`;
-} else if (watch.startsWith('/v/')) {
-    archive = true;
-    menuBox = 'div.select-type > ul.type';
-    wideBtn = 'div.bilibili-player-video-btn-widescreen';
-    wideStat = 'closed';
-    current = 'div.select-type > ul.type > li.active';
-    offset = `.jsui-video-menu {position: absolute;}`;
-} else {
-    menuBox = 'div.toolbar[mr-show]';
-    wideBtn = 'div.bpx-player-ctrl-wide';
-    wideStat = 'bpx-state-entered';
-    current = '[class*="numberListItem_select"]';
-    offset = `.jsui-video-menu {top: 16px; left: 520px;}`;
-}
-
-var jsUI = new JSUI();
-var observer = new NodeObserver();
-
-jsUI.css.add(`.jsui-video-menu {position: absolute; display: inline-block; z-index: 99999999999;}
-.jsui-video-menu > .jsui-menu-item {display: inline-block; width: 100px;}
-.jsui-options, .jsui-drop-menu {width: 140px;}
-.jsui-analyse {display: flex;}
-.jsui-menu-item {background-color: #c26; color: #fff; font-size: 16px; padding: 3px 5px !important; line-height: 26px;}
-.jsui-options, .jsui-analyse {position: absolute; border: 1px solid #000; padding: 5px; left: 100%; top: 0px; background-color: #fff;}
-.jsui-options * {font-size: 16px; text-align: center; padding: 5px; width: 100%; margin: 0px;}
-.jsui-options p, .jsui-options option:checked {color: #c26; font-weight: bold;} ${offset}`);
-
-var videoDL = jsUI.menu().class('jsui-video-menu');
-videoDL.add('设置').onclick(openOptions);
-videoDL.add('解析').onclick(analyseVideo);
-
-function openOptions() {
-    analyse_win.hide();
-    options_win.switch();
-}
-async function analyseVideo() {
-    if (worker || videocodec !== localStorage.videocodec) {
-        worker = false;
-        videocodec = localStorage.videocodec;
-        analyse_win.body();
-        if (bvplayer) {
-            let {title, pic, aid, cid} = document.defaultView.__INITIAL_STATE__.videoData;
-            biliVideoTitle(title);
-            biliVideoThumb(pic);
-            await biliVideoExtractor(cid, 'x/player/playurl?avid=' + aid + '&cid=' + cid, 'data');
-        }
-        else if (archive) {
-            let {aid, cid} = document.defaultView;
-            biliVideoTitle(document.querySelector('div.match-info-title').textContent);
-            await biliVideoExtractor(cid, 'x/player/playurl?avid=' + aid + '&cid=' + cid, 'data');
-        }
-        else {
-            let {name, thumbnailUrl} = JSON.parse(document.head.querySelector('script[type]').textContent).itemListElement[0];
-            let id = document.defaultView.__playinfo__.result.play_view_business_info.episode_info.ep_id;
-            biliVideoTitle(name);
-            biliVideoThumb(thumbnailUrl[0]);
-            await biliVideoExtractor(id, `pgc/player/web/playurl?ep_id=${id}`, 'result');
-        }
+const videoTypeHandlers = {
+    'video': {
+        bvPlayer: true,
+        menuBox: 'div.video-toolbar-left',
+        wideBtn: 'div.bpx-player-ctrl-wide',
+        wideStat: 'bpx-state-entered',
+        current: 'li.bpx-state-multi-active-item',
+    },
+    'v': {
+        bvArchive: true,
+        menuBox: 'div.select-type > ul.type',
+        wideBtn: 'div.bilibili-player-video-btn-widescreen',
+        wideStat: 'closed',
+        current: 'div.select-type > ul.type > li.active',
+    },
+    '!': {
+        menuBox: 'div.toolbar[mr-show]',
+        wideBtn: 'div.bpx-player-ctrl-wide',
+        wideStat: 'bpx-state-entered',
+        current: '[class*="numberListItem_select"]',
     }
-    options_win.hide();
-    analyse_win.switch();
-}
+};
+let bvType = videoTypeHandlers[bvWatch.match(/^\/(v(?:ideo)?)\//)?.[1]] ?? videoTypeHandlers['!'];
+
+const shortcutHandlers = {
+    'ArrowLeft': () => document.querySelector(bvType.current).previousElementSibling.click(),
+    'ArrowRight': () => document.querySelector(bvType.current).nextElementSibling.click()
+};
+
+window.addEventListener('play', async function biliVideoToolbar() {
+    let wide = await promiseSelector(bvType.wideBtn);
+    let menu = await promiseSelector(bvType.menuBox);
+    if (!wide.classList.contains(bvType.wideStat) && localStorage.autowide === '1' ) {
+        wide.click();
+    }
+    menu.append(mainPane, cssPane);
+    window.removeEventListener('play', biliVideoToolbar);
+}, true);
 
 document.addEventListener('keydown', (event) => {
-    var {ctrlKey, key} = event;
-    if (ctrlKey) {
-        if (key === 'ArrowLeft') {
-            event.preventDefault();
-            document.querySelector(current).previousElementSibling.click();
+    let handler = shortcutHandlers[event.key];
+    if (event.ctrlKey && handler) {
+        event.preventDefault();
+        handler();
+    }
+});
+
+let menuItem = document.createElement('div');
+menuItem.className = 'bili_video_button';
+
+let mainPane = document.createElement('div');
+mainPane.id = 'bili_video_main';
+mainPane.innerHTML = `
+<div id="bili_video_menu">
+    <div id="bili_video_optbtn" class="bili_video_button">设置</div>
+    <div id="bili_video_anabtn" class="bili_video_button">解析</div>
+</div>
+<div id="bili_video_options" class="bili_video_pane bili_video_hidden">
+    <h4>自动宽屏</h4>
+    <select name="autowide">
+        <option value="0">关闭</option>
+        <option value="1">启用</option>
+    </select>
+    <h4>编码格式</h4>
+    <select name="videocodec">
+        <option value="0">H.264</option>
+        <option value="1">HEVC</option>
+        <option value="2">AV-1</option>
+    </select>
+</div>
+<div id="bili_video_analyse" class="bili_video_pane bili_video_result bili_video_hidden"></div>
+`;
+
+let [menuPane, optionsPane, analysePane] = mainPane.children;
+
+const menuEventHandlers = {
+     'bili_video_optbtn': () => {
+         optionsPane.classList.toggle('bili_video_hidden');
+         analysePane.classList.add('bili_video_hidden');
+     },
+    'bili_video_anabtn': async () => {
+        optionsPane.classList.add('bili_video_hidden');
+        analysePane.classList.toggle('bili_video_hidden');
+        if (worker || videocodec !== localStorage.videocodec) {
+            worker = false;
+            videocodec = localStorage.videocodec;
+            analysePane.innerHTML = '';
+            if (bvType.bvPlayer) {
+                let {title, pic, aid, cid} = document.defaultView.__INITIAL_STATE__.videoData;
+                biliVideoTitle(title);
+                biliVideoThumb(pic);
+                await biliVideoExtractor(cid, 'x/player/playurl?avid=' + aid + '&cid=' + cid, 'data');
+            }
+            else if (bvType.bvArchive) {
+                let {aid, cid} = document.defaultView;
+                biliVideoTitle(document.querySelector('div.match-info-title').textContent);
+                await biliVideoExtractor(cid, 'x/player/playurl?avid=' + aid + '&cid=' + cid, 'data');
+            }
+            else {
+                let {name, thumbnailUrl} = JSON.parse(document.head.querySelector('script[type]').textContent).itemListElement[0];
+                let id = document.defaultView.__playinfo__.result.play_view_business_info.episode_info.ep_id;
+                biliVideoTitle(name);
+                biliVideoThumb(thumbnailUrl[0]);
+                await biliVideoExtractor(id, `pgc/player/web/playurl?ep_id=${id}`, 'result');
+            }
         }
-        else if (key === 'ArrowRight') {
-            event.preventDefault();
-            document.querySelector(current).nextElementSibling.click();
+    }
+};
+
+const codecHandlers = {
+    '0': 'bili_video_l264',
+    '1': 'bili_video_l265',
+    '2': 'bili_video_lav1'
+}
+
+function biliVideoTitle(name) {
+    let multi = document.querySelector(bvType.current)?.textContent?.trim();
+    name = multi ? `${name}-${multi}` : name;
+    bvTitle = name.trim().replace(/[\/\\:*?"<>|\s\r\n]/g, '_');
+}
+
+function biliVideoThumb(url) {
+    let thumb = menuItem.cloneNode(true);
+    thumb.classList.add('bili_video_thumb');
+    thumb.textContent = '视频封面';
+    thumb.url = url.replace(/^(https?:)?\/\//, 'https://');
+    thumb.file = bvTitle + url.slice(url.lastIndexOf('.'));
+    analysePane.appendChild(thumb);
+}
+
+async function biliVideoExtractor(vid, playurl, key) {
+    if (history[vid]) {
+        analysePane.innerHTML = '';
+        analysePane.append(...history[vid]);
+    } else {
+        let response = await fetch('https://api.bilibili.com/' + playurl + '&fnval=4050', {credentials: 'include'});
+        let json = await response.json();
+        let {video, audio} = json[key].dash;
+        [...video, ...audio].forEach(({id, codecs, baseUrl}) => {
+            let codec = codecs.slice(0, codecs.indexOf('.'));
+            let {text, ext} = format[id];
+            let {title, alt, type} = format[codec];
+            let menu = menuItem.cloneNode(true);
+            menu.classList.add('bili_video_' + type, 'bili_video_' + alt);
+            menu.textContent = text;
+            menu.title = title;
+            menu.url = baseUrl;
+            menu.file = bvTitle + ext;
+            analysePane.appendChild(menu);
+        });
+        history[vid] = [...analysePane.children];
+    }
+    analysePane.className = analysePane.className.replace(/\s?bili_video_l\w+/, '') + ' ' + codecHandlers[videocodec];
+}
+
+menuPane.addEventListener('click', (event) => {
+    let handler = menuEventHandlers[event.target.id];
+    if (handler) {
+        handler();
+    }
+});
+
+optionsPane.addEventListener('change', (event) => {
+    localStorage[event.target.name] = event.target.value;
+});
+
+analysePane.addEventListener('click', (event) => {
+    let {altKey, target: {url, file}} = event;
+    if (url && file) {
+        if (altKey) {
+            var urls = [{url, options: {out: file, referer: location.href} }];
+            window.postMessage({aria2c: 'aria2c_jsonrpc_call', params: urls});
+        }
+        else {
+            GM_download({url, responseType: 'blob', headers: {referer: location.href}, name: file});
         }
     }
 });
 
-window.addEventListener('play', async function biliVideoToolbar() {
-    var wide = await observer.timeout(wideBtn);
-    var menu = await observer.timeout(menuBox);
-    menu.append(videoDL);
-    if (!wide.classList.contains(wideStat) && localStorage.autowide === '1' ) {
-        wide.click();
-    }
-    window.removeEventListener('play', biliVideoToolbar);
-}, true);
+let [, optionWide,, optionCodec] = optionsPane.children;
+optionWide.value = autowide;
+optionCodec.value = videocodec;
+
+let cssPane = document.createElement('style');
+cssPane.textContent = `
+#bili_video_main {font-size: 16px; position: relative; text-align: center; padding-right: 5px; line-height: 28px;}
+#bili_video_menu {display: flex; gap: 5px;}
+.bili_video_button {border: outset 1px #000; padding: 3px; background-color: #c26; color: #fff; cursor: pointer; width: 100px;}
+.bili_video_button:hover {filter: contrast(80%);}
+.bili_video_button:active {filter: contrast(60%); border-style: inset;}
+.bili_video_pane {position: absolute; top: 0px; left: 100%; background-color: #fff; border: solid 1px #000; padding: 5px; z-index: 999;}
+.bili_video_pane > h4, .bili_video_pane > select {width: 110px !important; padding: 5px;}
+.bili_video_pane > h4 {color: #c26; font-weight: bold; margin: auto;}
+.bili_video_result {display: grid; grid-template-columns: 1fr 1fr 1fr; grid-auto-flow: dense; gap: 5px;}
+.bili_video_thumb {grid-column: 1;}
+.bili_video_video {grid-column: 2;}
+.bili_video_audio {grid-column: 3;}
+.bili_video_hidden {display: none;}
+.bili_video_l264 > .bili_video_video:not(.bili_video_h264), .bili_video_l265 > .bili_video_video:not(.bili_video_h265), .bili_video_lav1 > .bili_video_video:not(.bili_video_av1) {display: none;}
+`;
 
 new MutationObserver(mutations => {
-    if (watch !== location.pathname) {
-        watch = location.pathname;
+    if (bvWatch !== location.pathname) {
+        bvWatch = location.pathname;
         worker = true;
-        options_win.hide();
-        analyse_win.hide();
+        optionsPane.classList.add('bili_video_hidden');
+        analysePane.classList.add('bili_video_hidden');
     }
 }).observe(document.head, {childList: true});
 
-var options_win = jsUI.new().class('jsui-options').onchange(optionChange).hide().parent(videoDL);
-var wide_opt = jsUI.new('select').attr('id', 'autowide').body('<option value="0">关闭</option><option value="1">启用</option>');
-var code_opt = jsUI.new('select').attr('id', 'videocodec').body('<option value="0">H.264</option><option value="1">HEVC</option><option value="2">AV-1</option>');
-wide_opt.value = autowide;
-code_opt.value = videocodec;
-options_win.append(jsUI.new('p').body('自动宽屏'), wide_opt, jsUI.new('p').body('编码格式'), code_opt);
-
-var analyse_win = jsUI.new().class('jsui-analyse').hide().parent(videoDL);
-
-function optionChange(event) {
-    var {id, value} = event.target;
-    localStorage[id] = value;
-}
-
-function biliVideoTitle(name) {
-    var multi = document.querySelector(current);
-    name = multi ? `${name}-${multi.textContent}` : name;
-    title = name.trim().replace(/[\/\\:*?"<>|\s\r\n]/g, '_');
-}
-
-function biliVideoThumb(url) {
-    var thumb = jsUI.menu(true);
-    var fixed = url.replace(/^(https?:)?\/\//, 'https://');
-    var ext = url.slice(url.lastIndexOf('.'));
-    thumb.add('视频封面').onclick(event => downloadBiliVideo(event, fixed, ext));
-    analyse_win.append(thumb);
-}
-
-async function biliVideoExtractor(vid, playurl, key) {
-    var {menu, stream} = history[vid] ?? {};
-    if (!stream && !menu) {
-        var response = await fetch('https://api.bilibili.com/' + playurl + '&fnval=4050', {credentials: 'include'});
-        var json = await response.json();
-        menu = {avc1: jsUI.menu(true), hev1: jsUI.menu(true), av01: jsUI.menu(true), mp4a: jsUI.menu(true)};
-        stream = {avc1: [], hev1: [], av01: [], mp4a: []};
-        var {video, audio} = json[key].dash;
-        [...video, ...audio].forEach(({id, codecs, baseUrl}) => {
-            var codec = codecs.slice(0, codecs.indexOf('.'));
-            var {text, ext} = format[id];
-            var {title, alt} = format[codec];
-            menu[codec].add(text).attr('title', title).onclick(event => downloadBiliVideo(event.altKey, baseUrl, alt + ext));
-            stream[codec].push({url: baseUrl, name: alt + ext});
-        });
-        history[vid] = {menu, stream};
-    }
-    analyse_win.append(videocodec === '2' ? stream.av01.length !== 0 ? menu.av01 : stream.hev1.length !== 0 ? menu.hev1 : menu.avc1 : videocodec === '1' && stream.hev1.length !== 0 ? menu.hev1 : menu.avc1, menu.mp4a);
-}
-
-function downloadBiliVideo(altKey, url, ext) {
-    if (altKey) {
-        var urls = [{url, options: {out: title + ext, referer: location.href} }];
-        window.postMessage({aria2c: 'aria2c_jsonrpc_call', params: urls});
-    }
-    else {
-        GM_download({url, responseType: 'blob', headers: {referer: location.href}, name: title + ext});
-    }
+function promiseSelector(text, time = 1) {
+    return new Promise((resolve, reject) => {
+        time *= 5;
+        let t = setInterval(() => {
+            let node = document.querySelector(text);
+            if (node) {
+                resolve(node);
+            }
+            time --;
+            if (time === 0) {
+                clearInterval(t);
+                reject();
+            }
+        }, 200);
+    });
 }
