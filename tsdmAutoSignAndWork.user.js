684d81cb76caa623209e5c3faf469fd8f1f4b810
jc3213 <jc3213@users.noreply.github.com>
2025-06-09 14:04:05 +0800

    v1.4.1: use let rather than var

--- tsdmAutoSignAndWork.user.js
+++ tsdmAutoSignAndWork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         天使动漫自动签到打工
 // @namespace    https://github.com/jc3213/userscript
-// @version      1.4.0
+// @version      1.4.1
 // @description  天使动漫全自动打工签到脚本 — 完全自动无需任何操作，只需静待一分钟左右
 // @author       jc3213
 // @match        *://*.tsdm39.com/*
@@ -9,12 +9,12 @@
 // ==/UserScript==
 
 'use strict';
-var {signed = '0', worked = '0'} = localStorage;
-var action = {};
-var today = new Date();
-var date = today.getFullYear() + today.getMonth() + today.getDate();
-var now = today.getTime();
-var login = document.getElementById('lsform');
+let {signed = '0', worked = '0'} = localStorage;
+let action = {};
+let today = new Date();
+let date = today.getFullYear() + today.getMonth() + today.getDate();
+let now = today.getTime();
+let login = document.getElementById('lsform');
 
 if (!login) {
     if (date > signed) {
@@ -42,11 +42,12 @@ async function autoSign() {
         return;
     }
     action.signed = true;
-    var popup = startPopup('查询签到状态...', '80px');
-    var iframe = await startWork('/plugin.php?id=dsu_paulsign:sign');
-    var error = iframe.document.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)');
+    let text;
+    let popup = startPopup('查询签到状态...', '80px');
+    let iframe = await startWork('/plugin.php?id=dsu_paulsign:sign');
+    let error = iframe.document.querySelector('#ct_shell > div:nth-child(1) > h1:nth-child(1)');
     if (error) {
-        var text = error.innerText;
+        text = error.innerText;
     }
     else {
         popup.innerText = '开始签到...';
@@ -63,13 +64,14 @@ async function autoWork() {
     if (action.worked) {
         return;
     }
+    let text, next;
     action.worked = true;
     var popup = startPopup('查询打工状态...', '120px');
     var iframe = await startWork('/plugin.php?id=np_cliworkdz:work');
     if (iframe.document.querySelector('#messagetext')) {
-        var text = iframe.document.querySelector('#messagetext > p:nth-child(1)').innerHTML.split(/<br>|<script/)[1];
-        var [full, hh, mm, ss] = text.match(/(\d)小时(\d+)分钟(\d+)秒/);
-        var next = hh * 3600000 + mm * 60000 + ss * 1000;
+        text = iframe.document.querySelector('#messagetext > p:nth-child(1)').innerHTML.split(/<br>|<script/)[1];
+        let [full, hh, mm, ss] = text.match(/(\d)小时(\d+)分钟(\d+)秒/);
+        next = hh * 3600000 + mm * 60000 + ss * 1000;
     }
     else {
         popup.innerText = '开始打工...';
@@ -81,10 +83,9 @@ async function autoWork() {
         });
         await waitTimeout(3000);
         iframe.document.querySelector('#stopad > a').click();
-        setTimeout(autoWork, 21600000);
         text = '已完成打工';
         next = 21600000;
-
+        setTimeout(autoWork, next);
     }
     endWork('worked', Date.now() + next, text, popup, iframe.window);
 }
@@ -99,7 +100,7 @@ async function endWork(work, value, text, popup, frame) {
 }
 
 function startPopup(string, top) {
-    var popup = document.createElement('div');
+    let popup = document.createElement('div');
     popup.innerText = string;
     popup.style.cssText = 'border-radius: 5px; background-color: #FFF; padding: 5px; position: fixed; width: 380px; text-align: center; left: ' + (document.documentElement.clientWidth - 380) / 2 + 'px; font-size: 16px; top: ' + top;
     document.body.append(popup);
@@ -108,7 +109,7 @@ function startPopup(string, top) {
 
 function startWork(url) {
     return new Promise(resolve => {
-        var iframe = document.createElement('iframe');
+        let iframe = document.createElement('iframe');
         iframe.src = url;
         iframe.style.cssText = 'position: absolute; top: 400px; left: 400px; height: 400px; width: 400px; display: none;';
         iframe.addEventListener('load', event => resolve({document: iframe.contentDocument, window: iframe.contentWindow}));
