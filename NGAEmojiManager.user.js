338db1cfb26c2c7079995a661dc86bf0f0ae68d9
jc3213 <jc3213@users.noreply.github.com>
2025-11-10 21:14:37 +0800

    v2.0.0: 调整储存，优化UI分布

--- NGAEmojiManager.user.js
+++ NGAEmojiManager.user.js
@@ -2,7 +2,7 @@
 // @name            NGA Emoji Manager
 // @name:zh         NGA表情管理器
 // @namespace       https://github.com/jc3213
-// @version         1.9.1
+// @version         2.0.0
 // @description     Add/Remove New Emoji to NGA Forum
 // @description:zh  为NGA论坛添加/删除新表情
 // @author          jc3213
@@ -15,113 +15,105 @@
 // @match           *://nga.178.com/thread.php?*
 // @match           *://nga.178.com/read.php?*
 // @match           *://nga.178.com/post.php?*
-// @grant           GM_setValue
 // @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_deleteValue
+// @grant           GM_listValues
 // ==/UserScript==
 
-var emojiPackage = GM_getValue('emoji', []);
-var emojiRunOnce = {};
-var emojiTab;
-var emojiPanel;
-var emojiOrigin;
+let emojiPackage = GM_getValue('emoji');
+if (Array.isArray(emojiPackage)) {
+    emojiPackage.forEach(({ name, author, emoji }) => {
+        GM_setValue(name, { author, emoji });
+    });
+    GM_deleteValue('emoji');
+}
 
-var css = document.createElement('style');
-css.innerText = `.single_ttip2 img {max-height: 64px;}`
+let emojis = new Map( GM_listValues().map((i) => [i, GM_getValue(i)]) );
+let emojiUI = {};
+let emojiTab;
+let emojiPanel;
+
+let css = document.createElement('style');
+css.innerText = `.single_ttip2 img { max-height: 64px; }`
 document.head.append(css);
 
-var manager = document.createElement('button');
+let manager = document.createElement('button');
 manager.innerText = '添加新表情';
 manager.className = 'block_txt_big';
-manager.addEventListener('click', event => subscribe.click());
-
-var subscribe = document.createElement('input');
-subscribe.type = 'file';
-subscribe.accept = 'application/json';
-subscribe.multiple = 'true';
-subscribe.addEventListener('change', async event => {
-    var files = [...subscribe.files];
-    var result = files.map(async file => {
-        var json = await fileReader(file);
-        var find = emojiPackage.findIndex(package => package.name === json.name);
-        find = find === -1 ? emojiPackage.length : find;
-        emojiPackage[find] = json;
-        createEmojiUI(json, find);
-    });
-    await Promise.all(result);
-    GM_setValue('emoji', emojiPackage);
-    subscribe.value = '';
+manager.addEventListener('click', async (event) => {
+    let handles = await document.defaultView.showOpenFilePicker({ types: [ { description: 'JSON 文件', accept: { 'application/json': ['.json'] } } ], multiple: true });
+    Promise.all(handles.map(async (handler) => {
+        let file = await handler.getFile();
+        let text = await file.text();
+        let { name, emoji, author } = JSON.parse(text);
+        if (!name || !emoji) return;
+        emojis.set(name, { author, emoji });
+        GM_setValue(name, { author, emoji });
+        printEmojiUI(name, author, emoji);
+    }));
 });
 
-function createEmojiUI({name, author, emoji}, index) {
-    var panel = emojiPanel.childNodes[index + emojiOrigin];
-    if (panel) {
-        panel.innerHTML = '';
-        panel.style.display = 'none';
-        delete emojiRunOnce[name];
-    }
-    else {
-        var tab = document.createElement('button');
-        tab.className = 'block_txt_big';
-        tab.innerText = name;
-        tab.title = '制作者： ' + author;
-        manager.before(tab);
-        panel = document.createElement('div');
-        emojiPanel.append(panel);
-        tab.addEventListener('click', event => {
-            if (event.ctrlKey && confirm('确定要删除表情包【'+ name + '】吗？')) {
-                emojiPackage.splice(index, 1);
-                delete emojiRunOnce[name];
-                GM_setValue('emoji', emojiPackage);
-                tab.remove();
-                panel.remove();
-            }
-            else {
-                emojiPanel.childNodes.forEach(ePanel => {
-                    ePanel.style.display = ePanel === panel ? 'block' : 'none';
-                });
-                appendEmojiToUI(name, panel);
-            }
-        });
-    }
+function printEmojiUI(name, author, emoji) {
+    let panel = emojiUI[name] ??= createEmojiUI(name, author, emoji);
+    panel.innerHTML = '';
+    panel.style.display = 'none';
 }
 
-function appendEmojiToUI(name, panel) {
-    if (emojiRunOnce[name]) {
-        return;
-    }
-    var {emoji} = emojiPackage.find(package => package.name === name);
-    emoji.forEach(em => {
-        var img = document.createElement('img');
-        img.src = 'https://img.nga.178.com/attachments/' + em;
-        img.addEventListener('click', event => {
-            postfunc.addText('[img]' + em + '[/img]');
-            postfunc.selectSmilesw._.hide();
-        });
-        panel.appendChild(img);
-    });
-    emojiRunOnce[name] = true;
-}
-
-function fileReader(file) {
-    return new Promise(resolve => {
-        var reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = () => {
-            var json = JSON.parse(reader.result);
-            resolve(json);
-        };
-    });
-}
-
-new MutationObserver(mutationList => {
-    mutationList.forEach(mutation => {
-        var newNode = mutation.addedNodes[0];
-        if (newNode && newNode.id.startsWith('commonwindow')) {
-            emojiTab = newNode.querySelector('.div1 > .div2 > .div3 > div');
-            emojiPanel = newNode.querySelector('.div1 > .div2 > .div3 > span');
-            emojiOrigin = emojiPanel.childNodes.length;
-            emojiTab.appendChild(manager);
-            emojiPackage.forEach(createEmojiUI);
+function createEmojiUI(name, author, emoji) {
+    let tab = document.createElement('button');
+    tab.className = 'block_txt_big';
+    tab.innerText = name;
+    tab.title = author ? '制作者： ' + author : '';
+    manager.before(tab);
+    let panel = document.createElement('div');
+    emojiPanel.append(panel);
+    tab.addEventListener('click', event => {
+        if (event.ctrlKey && confirm('确定要删除表情包【'+ name + '】吗？')) {
+            GM_deleteValue(name)
+            emojis.delete(name);
+            tab.remove();
+            panel.remove();
+            delete emojiUI[name];
+        }
+        else {
+            emojiPanel.childNodes.forEach((pane) => { pane.style.display = 'none'; });
+            panel.style.display = '';
+            appendEmojiToUI(name, emoji, panel);
         }
     });
-}).observe(document.body, {childList: true});
+    return panel;
+}
+
+function appendEmojiToUI(name, emoji, panel) {
+    if (panel.runOnce) return;
+    panel.runOnce = true;
+    emoji.forEach((em) => {
+        let img = document.createElement('img');
+        img.src = 'https://img.nga.178.com/attachments/' + em;
+        img.alt = em;
+        panel.appendChild(img);
+    });
+    panel.addEventListener('click', (event) => {
+        let { alt } = event.target;
+        if (!alt) return;
+        postfunc.addText(`[img]${alt}[/img]`);
+        postfunc.selectSmilesw._.hide();
+    });
+}
+
+let observer = new MutationObserver((mutationList) => {
+    for (let mutation of mutationList) {
+        for (let node of mutation.addedNodes) {
+            if (node?.id && node.id.startsWith('commonwindow')) {
+                let emojiMain = node.children[0].children[1].children[0].children;
+                emojiTab = emojiMain[0];
+                emojiPanel = emojiMain[1];
+                emojiTab.appendChild(manager);
+                emojis.forEach(({ author, emoji }, name) => printEmojiUI(name, author, emoji));
+                return observer.disconnect();
+            }
+        }
+    }
+});
+observer.observe(document.body, { childList: true });
