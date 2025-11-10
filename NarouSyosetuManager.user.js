ede36f3654592ca60efc13cc7632ae41247c6289
jc3213 <jc3213@users.noreply.github.com>
2025-11-10 19:22:43 +0800

    v2.1.0: 増量更新

--- NarouSyosetuManager.user.js
+++ NarouSyosetuManager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         「小説家になろう」 書庫管理
 // @namespace    https://github.com/jc3213/userscript
-// @version      2.0.0
+// @version      2.1.0
 // @description  「小説家になろう」の小説情報を管理し、縦書きPDFをダウンロードするツールです
 // @author       jc3213
 // @match        https://ncode.syosetu.com/*
@@ -11,6 +11,8 @@
 // @exclude      https://ncode.syosetu.com/novelreview/*
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
 // ==/UserScript==
 
 'use strict';
@@ -18,19 +20,48 @@
 let [, novelcode, novelread] = location.pathname.match(/^\/(?:.*\/ncode\/)?(n\w+)\/(?:(\d+))?\/?$/);
 if (!novelcode) return;
 
-let shortcut = '';
-let shortime;
-let novelname = $('.c-announce-box > :last-child > :first-child, h1.novel_title')[0].textContent;
-let myncode = novelcode;
-let now = new Date();
-let today = now.getFullYear() + now.getMonth() + now.getDate();
-let timeline = now.getTime();
+let bookmark = GM_getValue('bookmark');
+if (bookmark) {
+    bookmark.forEach(({ ncode, title }) => {
+        GM_setValue(ncode, title);
+    });
+    GM_deleteValue('bookmark');
+}
+bookmark = GM_listValues();
+let novels = new Map( bookmark.map((i) => [i, GM_getValue(i)]) );
+
 let validate = {};
-let novels = {};
-let changed = false;
+let shortcut = '';
+let novelname = $('.c-announce-box > :last-child > :first-child, h1.novel_title')[0].textContent;
 let shelf = false;
-let bookmark = GM_getValue('bookmark', []);
-let scheduler = localStorage.scheduler ?? today;
+
+// ホットキー関連
+let keyTime;
+
+const hotkey = {
+    '0' () {
+        clearTimeout(keyTime);
+        shortcut += event.key;
+        keyTime = setTimeout(() => {
+            open('https://ncode.syosetu.com/' + novelcode + (shortcut[0] === '0' ? '/' : '/' + shortcut + '/'), '_blank');
+            shortcut = '';
+        }, 1000);
+    },
+    'Escape': () => clearTimeout(keyTime),
+    'ArrowLeft': () => $('a.c-pager__item--before')[0]?.click(),
+    'ArrowRight': () => $('a.c-pager__item--next')[0]?.click()
+};
+
+['1','2','3','4','5','6','7','8','9'].forEach((i) => {
+    hotkey[i] = hotkey['0'];
+});
+
+$(document).keydown((event) => {
+    let key = hotkey[event.key];
+    if (!key || event.target.localName === 'input') return;
+    event.preventDefault();
+    hotkey[event.key]();
+});
 
 // UI作成関連
 let manager = $('<div class="jsui-basic-menu jsui-book-manager"></div>');
@@ -41,7 +72,6 @@ let css = $(`<style>
 .jsui-menu-item:not(.jsui-menu-disabled):hover, .jsui-button-cell:hover { cursor: pointer; filter: contrast(65%); }
 .jsui-menu-item:not(.jsui-menu-disabled):active, .jsui-button-cell:active { filter: contrast(35%); }
 .jsui-menu-checked { padding: 4px 9px; border-style: inset; border-width: 1px; }
-.jsui-menu-disabled { filter: contrast(15%); }
 .jsui-basic-menu { margin: 0px; padding: 0px; user-select: none; display: flex; gap: 1px; }
 .jsui-basic-menu > input { flex: 2; }
 .jsui-book-manager { position: relative; font-weight: bold; left: 5px; width: fit-content; }
@@ -60,7 +90,7 @@ p { margin: 0px; font-size: 18px !important; font-family: "Segoe UI", Verdana, "
 
 let button = $('<div class="jsui-menu-item">書庫管理</div>').click(() => {
     if (shelf === false) {
-        bookmark.forEach(fancyTableItem);
+        novels.forEach(fancyTableItem);
         shelf = true;
     }
     bookshelf.toggle();
@@ -90,125 +120,105 @@ function removeHeaderFooter() {
     }
 };
 
-// ショットカットマッピング
-const hotkey = {
-    '0' (event) {
-        event.preventDefault();
-        clearTimeout(shortime);
-        shortcut += event.key;
-        shortime = setTimeout(() => {
-            open('https://ncode.syosetu.com/' + novelcode + (shortcut[0] === '0' ? '/' : '/' + shortcut + '/'), '_blank');
-            shortcut = '';
-        }, 1000);
-    },
-    'Escape': () => clearTimeout(shortime),
-    'ArrowLeft': (event) => {
-        event.preventDefault();
-        $('a.c-pager__item--before')[0]?.click();
-    },
-    'ArrowRight': (event) => {
-        event.preventDefault();
-        $('a.c-pager__item--next')[0]?.click();
-    }
-};
-['1','2','3','4','5','6','7','8','9'].forEach((i) => { hotkey[i] = hotkey['0']; });
-
-$(document).keydown((event) => {
-    hotkey[event.key]?.(event);
-});
-
 // 書庫管理UI
 let bookshelf = $('<div class="jsui-book-shelf"></div>').hide();
 let shelf_menu = $('<div class="jsui-basic-menu"></div>');
 let shelf_table = $('<div class="jsui-table"></div>').html('<div class="jsui-table-head jsui-table-column"><div class="jsui-table-cell">NCODE</div><div class="jsui-table-cell">小説タイトル</div></div>');
 bookshelf.append(shelf_menu, shelf_table);
 
-let input_field = $('<input>').change((event) => {
-    myncode = event.target.value ?? novelcode;
+let input_field = $('<input>').keydown((event) => {
+    if (event.key === 'Enter') {
+        submit_btn[0].click();
+    }
 });
 
 let submit_btn = $('<div class="jsui-menu-item">NCODE登録</div>').click(async () => {
-    let book = bookmark.find(({ncode}) => ncode === myncode);
-    if (book) {
-        let {ncode, title} = book;
-        fancyPopup(ncode, title, 'は既に書庫に登録しています！');
-        validate[ncode] = title;
+    let ncode = input_field[0].value.match(/\/(n\w+)\//)?.[1];
+    if (!ncode) {
+        return fancyPopup(ncode, null, 'は間違っています！');
     }
-    else if (myncode === novelcode) {
-        subscribeNcode(novelcode, novelname);
+    let book = novels.get(ncode);
+    if (book) {
+        fancyPopup(ncode, book, 'は既に書庫に登録しています！');
+    }
+    else if (ncode === novelcode) {
+        subscribeNcode(ncode, novelname);
     }
     else {
-        if (validate[myncode]) {
-            return fancyPopup(myncode, null, 'が存在しません！');;
+        if (validate[ncode]) {
+            return fancyPopup(ncode, null, 'を検証しています、しばらくお待ちください！');
         }
-        validate[myncode] = '検証中';
-        fancyPopup(myncode, null, 'を検証しています、しばらくお待ちください！');
-        let res = await fetch('/' + myncode);
+        validate[ncode] = true;
+        let res = await fetch('/' + ncode);
         let text = await res.text();
-        if (text.includes('class="nothing"')) {
-            validate[myncode] = 'が存在しません';
-        }
-        else {
-            let tl = text.indexOf('<title>');
-            let te = text.indexOf('</title>');
-            title = text.slice(tl + 7, te);
-            subscribeNcode(myncode, title)
-        }
+        book = text.match(/<title[^>]*>(.*)<\/title>/)[1];
+        book === 'エラー'
+            ? fancyPopup(ncode, null, 'は「小説家になろう」に存在しません！')
+            : subscribeNcode(ncode, book)
+        delete validate[ncode];
     }
 });
-function subscribeNcode(ncode, title) {
-    let book = {ncode, title, last: 0, next: 0};
-    fancyTableItem(book, bookmark.length);
-    bookmark.push(book);
-    saveBookmarkButton();
-    fancyPopup(ncode, title, 'は書庫に登録しました！');
+function subscribeNcode(ncode, book) {
+    fancyTableItem(book, ncode);
+    novels.set(ncode, book);
+    GM_setValue(ncode, book);
+    fancyPopup(ncode, book, 'は書庫に登録しました！');
 }
-let update_btn = $('<div class="jsui-menu-item jsui-menu-disabled" id="jsui-save-btn">書庫更新</div>').click(() => {
-    if (!changed) return;
-    GM_setValue('bookmark', bookmark);
-    update_btn.addClass('jsui-menu-disabled');
-    changed = false;
-    shelf_table.show();
+
+let types = [{ description: 'なろう書庫', accept: { 'application/x-syosetu': ['.syosetu'] } }];
+let export_btn = $('<div class="jsui-menu-item" id="jsui-save-btn">書庫エクスポート</div>').click(async () => {
+    let text = JSON.stringify([...novels], null, 4);
+    let blob = new Blob([text]);
+    let now = new Date();
+    let time = now.getFullYear() + paddingTimeStamp(now.getMonth() + 1) + paddingTimeStamp(now.getDate()) + '-' + paddingTimeStamp(now.getHours()) + paddingTimeStamp(now.getMinutes()) + paddingTimeStamp(now.getSeconds());
+    let handle = await document.defaultView.showSaveFilePicker({ suggestedName: 'なろう書庫-' + time + '.syosetu', types });
+    let writer = await handle.createWritable();
+    await writer.write(blob);
+    await writer.close();
 });
-shelf_menu.append(input_field, submit_btn, update_btn);
+let import_btn = $('<div class="jsui-menu-item" id="jsui-save-btn">書庫インスポート</div>').click(async () => {
+    let [handle] = await document.defaultView.showOpenFilePicker({ types });
+    let file = await handle.getFile();
+    let text = await file.text();
+    let json = JSON.parse(text);
+    novels = new Map(json);
+    shelf_table.empty();
+    novels.forEach((book, ncode) => {
+        GM_setValue(ncode, book);
+        fancyTableItem(book, ncode);
+    });
+});
+function paddingTimeStamp(number) {
+    return String(number).padStart(2, '0');
+}
+
+shelf_menu.append(input_field, submit_btn, export_btn, import_btn);
 $(document.body).append(bookshelf, overlay);
 
 // 書庫表記生成
-function fancyTableItem(book, index) {
-    let { ncode, title, next, last } = book;
+function fancyTableItem(book, ncode) {
     let mybook = $('<div class="jsui-table-column"></div>').attr('id', ncode);
-    let cell_ncode = $('<div class="jsui-button-cell"></div>').text(ncode).click(() => removeNcodeFromShelf(mybook, index, ncode, title));
-    let cell_title = $('<div class="jsui-button-cell"></div>').text(title).click(() => openNcodeInNewPage(ncode, title));
+    let cell_ncode = $('<div class="jsui-button-cell"></div>').text(ncode).click(() => removeNcodeFromShelf(mybook, ncode, book));
+    let cell_title = $('<div class="jsui-button-cell"></div>').text(book).click(() => openNcodeInNewPage(ncode, book));
     mybook.append(cell_ncode, cell_title).appendTo(shelf_table);
 }
-function removeNcodeFromShelf(mybook, index, ncode, title) {
-    if (!confirm('【 ' + title + ' 】を書庫から削除しますか？')) return;
+function removeNcodeFromShelf(mybook, ncode, book) {
+    if (!confirm('【 ' + book + ' 】を書庫から削除しますか？')) return;
     mybook.remove();
-    bookmark.splice(index, 1);
-    saveBookmarkButton();
-    fancyPopup(ncode, title, 'は書庫から削除しました！');
+    novels.delete(ncode);
+    GM_deleteValue(ncode);
+    fancyPopup(ncode, book, 'は書庫から削除しました！');
 }
-function openNcodeInNewPage(ncode, title) {
-    if (!confirm('小説【 ' + title + ' 】を開きますか？')) return;
+function openNcodeInNewPage(ncode, book) {
+    if (!confirm('小説【 ' + book + ' 】を開きますか？')) return;
     open('https://ncode.syosetu.com/' + ncode + '/', '_blank');
-}
-function saveBookmarkButton() {
-    update_btn.removeClass('jsui-menu-disabled');
-    changed = true;
 }
 
 // ログ関連
-function fancyPopup(ncode, title, result) {
-    let message;
-    if (ncode && title) {
-        message = '「<span style="color: darkgreen" title="Nコード【' + ncode + '】">' + title + '</span>」 <span style="color: violet">' + result + '</span>';
-    }
-    else if (ncode === undefined && title) {
-        message = '<span style="color: darkgreen">' + title + '</span> <span style="color: violet">' + result + '</span>';
-    }
-    else if (ncode && title === undefined) {
-        message = '<span style="color: darkgreen">' + ncode + '</span> <span style="color: violet">' + result + '</span>';
-    }
+function fancyPopup(ncode, book, result) {
+    let message = book
+        ? '「<span style="color: darkgreen" title="Nコード【' + ncode + '】">' + book + '</span>」 <span style="color: violet">' + result + '</span>'
+        : '<span style="color: darkgreen">' + ncode + '</span> <span style="color: violet">' + result + '</span>';
     let popup = $('<div class="jsui-notify-popup"></div>').html(message).click(() => popup.remove());
     overlay.append(popup);
     popup.css('left', ($(document).width() - popup.width()) / 2 + 'px');
